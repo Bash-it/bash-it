c1d3117ad1dcf4f0a13ce521c5e1c5835deeca06
Robert R Evans <robert@codewranglers.org>
2010-10-02 12:31:01 -0700

    Better naming for base.bash colors

--- themes/base.bash
+++ themes/base.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Some Colors
-BGREEN='\[\033[1;32m\]'
+BOLD_GREEN='\[\033[1;32m\]'
 GREEN='\[\033[0;32m\]'
-BRED='\[\033[1;31m\]'
+BOLD_RED='\[\033[1;31m\]'
 RED='\[\033[0;31m\]'
-BBLUE='\[\033[1;34m\]'
+BOLD_BLUE='\[\033[1;34m\]'
 BLUE='\[\033[0;34m\]'
 PINK='\[\e[37;1;35m\]'
 NORMAL='\[\033[00m\]'
