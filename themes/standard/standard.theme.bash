eeaef1bada32a42c41d6db4ee40ff1a0443b0872
Robert R Evans <robert@codewranglers.org>
2010-10-06 17:28:34 -0700

    Added standard theme from alsemyonov.

---
+++ themes/standard/standard.theme.bash
@@ -0,0 +1,9 @@
+PROMPT='\[${green}\]\u\[${normal}\]@\[${green}\]\h\[${normal}\]:\[${blue}\]\w\[${normal}\]\[${red}\]$(prompt_char)$(git_prompt_info)\[${normal}\]\$ '
+
+# git themeing
+GIT_THEME_PROMPT_DIRTY="×"
+GIT_THEME_PROMPT_CLEAN="✓"
+GIT_THEME_PROMPT_PREFIX=""
+GIT_THEME_PROMPT_SUFFIX=""
+
+# ${debian_chroot:+($debian_chroot)}
