63d5e7be5ecf193a641bad74e85fd5434f197b7f
Rich Manalang <rich.manalang@gmail.com>
2010-10-07 13:19:53 -0700

    update the tab/window title with the pwd

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # prompt themeing
-PROMPT="${bold_blue}\$(prompt_char)\$(git_prompt_info) ${orange}\h ${reset_color}in ${green}\w ${reset_color}→ "
+
+#added TITLEBAR for updating the tab and window titles with the pwd
+case $TERM in
+	xterm*)
+	TITLEBAR='\[\033]0;\w\007\]'
+	;;
+	*)
+	TITLEBAR=""
+	;;
+esac
+PROMPT="${TITLEBAR}${bold_blue}\$(prompt_char)\$(git_prompt_info) ${orange}\h ${reset_color}in ${green}\w ${reset_color}→ "
 
 
 # git themeing
