b64c228da512350701af84f123241c46b694a00b
Robert R Evans <robert@codewranglers.org>
2010-10-07 17:15:14 -0700

    Updates

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# prompt themeing
+
+PROMPT="${bold_blue}\[\$(prompt_char)\]\[\$(git_prompt_info)\] ${orange}\h ${reset_color}in ${green}\w ${reset_color}\[→ "
+
+
+# git theming
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+# GIT_THEME_PROMPT_DIRTY=" ✗"
+# GIT_THEME_PROMPT_CLEAN=" ✓"
+# GIT_THEME_PROMPT_PREFIX=" |"
+# GIT_THEME_PROMPT_SUFFIX="|"
+
 
 #added TITLEBAR for updating the tab and window titles with the pwd
 case $TERM in
@@ -11,16 +25,3 @@ case $TERM in
 	TITLEBAR=""
 	;;
 esac
-PROMPT="${TITLEBAR}${bold_blue}\$(prompt_char)\$(git_prompt_info) ${orange}\h ${reset_color}in ${green}\w ${reset_color}→ "
-
-
-# git themeing
-# GIT_THEME_PROMPT_DIRTY=" ${red}✗"
-# GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-# GIT_THEME_PROMPT_PREFIX=" ${green}|"
-# GIT_THEME_PROMPT_SUFFIX="${green}|"
-
-GIT_THEME_PROMPT_DIRTY=" ✗"
-GIT_THEME_PROMPT_CLEAN=" ✓"
-GIT_THEME_PROMPT_PREFIX=" |"
-GIT_THEME_PROMPT_SUFFIX="|"
