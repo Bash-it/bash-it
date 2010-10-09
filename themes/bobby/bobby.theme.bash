f1ead0535bccbbffcbd6792f51071f5cd6b97181
Robert R Evans <robert@codewranglers.org>
2010-10-09 16:28:16 -0700

    updated bobby theme

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -3,17 +3,11 @@
 
 PROMPT="${bold_blue}\[\$(prompt_char)\]\[\$(git_prompt_info)\] ${orange}\h ${reset_color}in ${green}\w ${reset_color}\[→ "
 
-
 # git theming
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 GIT_THEME_PROMPT_PREFIX=" ${green}|"
 GIT_THEME_PROMPT_SUFFIX="${green}|"
-
-# GIT_THEME_PROMPT_DIRTY=" ✗"
-# GIT_THEME_PROMPT_CLEAN=" ✓"
-# GIT_THEME_PROMPT_PREFIX=" |"
-# GIT_THEME_PROMPT_SUFFIX="|"
 
 
 #added TITLEBAR for updating the tab and window titles with the pwd
