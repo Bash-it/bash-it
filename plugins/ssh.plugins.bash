e1c063ff0aa495452fc4798c7dea900eaf8c07b2
Robert R Evans <robert@codewranglers.org>
2010-10-10 17:54:11 -0700

    Updated the ssh config

--- plugins/ssh.plugins.bash
+++ plugins/ssh.plugins.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function add_ssh() {
-  echo -en "\n\nHost $1\n  HostName $2\n  User $3" >> ~/.ssh/config
+  echo -en "\n\nHost $1\n  HostName $2\n  User $3\n  ServerAliveInterval 30\n  ServerAliveCountMax 120" >> ~/.ssh/config
 }
