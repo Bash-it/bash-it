50b19d8bef6cad2daa0ede1912557d72064cc9a9
Robert R Evans <robert@codewranglers.org>
2010-10-12 10:11:21 -0700

    Added sshosts to view all the ssh config hosts that have been added. This command will only print out the host names that you would use for ssh/scp.

--- plugins/ssh.plugins.bash
+++ plugins/ssh.plugins.bash
@@ -3,3 +3,7 @@
 function add_ssh() {
   echo -en "\n\nHost $1\n  HostName $2\n  User $3\n  ServerAliveInterval 30\n  ServerAliveCountMax 120" >> ~/.ssh/config
 }
+
+function sshosts() {
+  awk '$1 ~ /Host$/ { print $2 }' ~/.ssh/config
+}
