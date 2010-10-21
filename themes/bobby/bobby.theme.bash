7aa791e2d6a161e57d6fc2b2e445d1dc5fc7088d
Robert R Evans <robert@codewranglers.org>
2010-10-21 15:06:32 -0700

    Improved the wrapping of the bobby theme

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-PROMPT="${bold_blue}\[\$(prompt_char)\]\[\$(git_prompt_info)\]${blue}\$(rvm_version_prompt) ${orange}\h ${reset_color}in ${green}\w ${reset_color}\[→ "
+PROMPT="\[${bold_blue}\]\[\$(prompt_char)\]\[\$(git_prompt_info)\]\[${blue}\]\[\$(rvm_version_prompt) \]\[${orange}\]\h \[${reset_color}\]in \[${green}\]\w \[${reset_color}\]\[→ "
 
 # git theming
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
