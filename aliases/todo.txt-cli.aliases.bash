32bbc80069c2b1e5af065bb0a0b182e5e6481a8b
Mark Szymanski <mrman208@me.com>
2010-10-31 15:28:06 -0500

    Added help for todo.txt-cli aliases.

--- aliases/todo.txt-cli.aliases.bash
+++ aliases/todo.txt-cli.aliases.bash
@@ -5,3 +5,15 @@ alias ta="$TODO a"
 alias trm="$TODO rm"
 alias tdo="$TODO do"
 alias tpri="$TODO pri"
+
+todo-help() {
+	echo
+	echo "todo.txt-cli Custom Aliases Usage"
+	echo
+	echo " tls  = $TODO ls"
+	echo " ta   = $TODO add"
+	echo " trm  = $TODO rm"
+	echo " tdo  = $TODO do"
+	echo " tpri = $TODO pri"
+	echo
+}
