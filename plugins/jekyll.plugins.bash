a8fc3c78d9d137f4468d89c76b95412a66b0ed18
Mark Szymanski <mrman208@me.com>
2010-11-01 20:24:39 -0500

    Added jekyll.plugins.bash file

---
+++ plugins/jekyll.plugins.bash
@@ -0,0 +1,12 @@
+newpost() {
+
+	# Get the date for the new post's filename
+
+	FNAME_DATE=$(date "+%Y-%m-%d")
+
+	# Get the title for the new post
+
+	read -p "Enter title of the new post: " POST_TITLE
+
+
+}
