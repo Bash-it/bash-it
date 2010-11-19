7f721fe5e7bc9d845d177beb044c096d95b155b8
Fedyashev Nikita <loci.master@gmail.com>
2010-11-19 00:27:14 -0500

    completion to help command added

--- plugins/vagrant.plugins.bash
+++ plugins/vagrant.plugins.bash
@@ -3,23 +3,29 @@ _vagrant()
 {
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-
+    commands="box destroy halt help init package provision reload resume ssh ssh_config status suspend up version"
 
     if [ $COMP_CWORD == 1 ]
     then
-      commands="box destroy halt help init package provision reload resume ssh ssh_config status suspend up version"
       COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
       return 0
     fi
 
     if [ $COMP_CWORD == 2 ]
     then
-      if [ $prev == 'box' ]
-      then
-          commands="add help list remove repackage"
-          COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
-          return 0
-      fi
+        case "$prev" in
+            "box")
+              box_commands="add help list remove repackage"
+              COMPREPLY=($(compgen -W "${box_commands}" -- ${cur}))
+              return 0
+            ;;
+            "help")
+              COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
+              return 0
+            ;;
+            *)
+            ;;
+        esac
     fi
 
     if [ $COMP_CWORD == 3 ]
@@ -29,8 +35,8 @@ _vagrant()
       then
         case "$prev" in
             "remove"|"repackage")
-              local vagrantlist=$(find $HOME/.vagrant/boxes/* -maxdepth 0 -type d -printf '%f ')
-              COMPREPLY=($(compgen -W "${vagrantlist}" -- ${cur}))
+              local box_list=$(find $HOME/.vagrant/boxes/* -maxdepth 0 -type d -printf '%f ')
+              COMPREPLY=($(compgen -W "${box_list}" -- ${cur}))
               return 0
               ;;
             *)
