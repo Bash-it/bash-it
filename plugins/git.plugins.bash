aaa107161bd7dd12517c30aa3f59b07878090e7f
Florian Baumann <flo@noqqe.de>
2010-11-22 13:07:08 +0100

    added git_info function to git plugin

--- plugins/git.plugins.bash
+++ plugins/git.plugins.bash
@@ -18,3 +18,39 @@ function git_remove_missing_files() {
 function local-ignore() {
   echo "$1" >> .git/info/exclude
 }
+
+# get a quick overview for your git repo
+function git_info() {
+    if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
+        # print informations
+        echo "git repo overview"
+        echo "-----------------"
+        echo
+
+        # print all remotes and thier details
+        for remote in $(git remote show); do
+            echo $remote:  
+            git remote show $remote
+            echo
+        done
+
+        # print status of working repo
+        echo "status:"
+        if [ -n "$(git status -s 2> /dev/null)" ]; then
+            git status -s
+        else
+            echo "working directory is clean"
+        fi
+
+        # print at least 5 last log entries
+        echo 
+        echo "log:"
+        git log -5 --oneline
+        echo 
+
+    else
+        echo "you're currently not in a git repository"
+
+    fi
+}
+
