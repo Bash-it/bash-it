e5536385777f1f18960a8d89868214521ceaef83
Mark Szymanski <mrman208@me.com>
2010-12-05 15:49:51 -0600

    Added colors to 'modern' theme

--- themes/modern/modern.theme.bash
+++ themes/modern/modern.theme.bash
@@ -1,6 +1,12 @@
 SCM_THEME_PROMPT_PREFIX=""
 SCM_THEME_PROMPT_SUFFIX=""
 
+SCM_THEME_PROMPT_DIRTY=' ${bold_red}✗${normal}'
+SCM_THEME_PROMPT_CLEAN=' ${bold_green}✓${normal}'
+SCM_GIT_CHAR='${bold_green}±${normal}'
+SCM_SVN_CHAR='${bold_cyan}⑆${normal}'
+SCM_HG_CHAR='${bold_red}☿${normal}'
+
 modern_scm_prompt() {
 	CHAR=$(scm_char)
 	if [ $CHAR = $SCM_NONE_CHAR ]
