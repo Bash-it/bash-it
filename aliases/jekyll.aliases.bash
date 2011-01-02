2c988b85bd400bb3f3142c0ea406180ed035f984
Mark Szymanski <mrman208@me.com>
2011-01-01 18:07:49 -0600

    Removed old code and added better indentation

--- aliases/jekyll.aliases.bash
+++ aliases/jekyll.aliases.bash
@@ -1,18 +1,11 @@
 # Open the root of your site in your vim or builtin cd to it
 
 if [[ $EDITOR = "vim" ]]
-then alias newentry="builtin cd $JEKYLL_LOCAL_ROOT && $EDITOR ."
-else alias newentry="builtin cd $JEKYLL_LOCAL_ROOT"
+then 
+	alias newentry="builtin cd $JEKYLL_LOCAL_ROOT && $EDITOR ."
+else 
+	alias newentry="builtin cd $JEKYLL_LOCAL_ROOT"
 fi
-
-# Open the _posts/ directory for making a new blog post (seperate from above alias because not everyone uses jekyll for a blog)
-
-# if [ $editor = "vim" ]
-# then
-# 	alias newpost="builtin cd $jekyll_local_root/_posts && $editor ."
-# else
-# 	alias newpost="builtin cd $jekyll_local_root"
-# fi
 
 # Build and locally serve the site
 
