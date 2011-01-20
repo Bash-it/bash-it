b4d773441b3565fba84f4af2c79e83d309283c51
Mark Szymanski <mrman208@me.com>
2011-01-20 11:05:50 -0600

    Added 'hex' alias to open file with Hex Fiend

--- aliases/osx.aliases.bash
+++ aliases/osx.aliases.bash
@@ -13,6 +13,7 @@ alias chromium="open -a chromium"
 alias dashcode="open -a dashcode"
 alias f='open -a Finder '
 alias textedit='open -a TextEdit'
+alias hex='open -a "Hex Fiend"'
 
 if [ -s /usr/bin/firefox ] ; then
   unalias firefox
