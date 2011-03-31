97e73f44dc345555008fba8c400dd860ee2cf084
Travis Swicegood <development@domain51.com>
2011-03-31 17:01:33 -0500

    add support for mactex if its present

---
+++ plugins/latex.plugin.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# add mactex to the path if its present
+MACTEX_PATH=/usr/local/texlive/2009/bin/universal-darwin
+if [[ -d  $MACTEX_PATH ]]; then
+    export PATH=$PATH:$MACTEX_PATH
+fi
