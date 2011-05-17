b1e19cfe3e0d975d4d9079d8513b801158ce929e
Mark Szymanski <mrman208@me.com>
2011-05-16 22:03:57 -0500

    Changed up minimal theme
    Removed Vim prompt stuff and made the prompt cyan

--- themes/minimal/minimal.theme.bash
+++ themes/minimal/minimal.theme.bash
@@ -1,19 +1,5 @@
 prompt_setter() {
-	if [[ $? -eq 0 ]]; then
-		if [ ! $VIMRUNTIME = "" ]
-		then
-			PS1="{vim} \W "
-		else
-			PS1="\W "
-		fi
-	else
-		if [ ! $VIMRUNTIME = "" ]
-		then
-			PS1="{vim} ${bold_red}\W ${normal}"
-		else
-			PS1="${bold_red}\W ${normal}"
-		fi
-	fi
+  PS1="${cyan}\W${normal} "
 }
 
 PROMPT_COMMAND=prompt_setter
