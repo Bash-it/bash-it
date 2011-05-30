20d5f97f3e14545a120c1ade2c0404943fec8e41
Travis Swicegood <development@domain51.com>
2011-05-30 09:46:22 -0500

    Load RVM and auto-completion is available
    No reason not to turn on RVM if it's present and the autocompletion is a
    huge benefit.

--- plugins/available/rvm.plugin.bash
+++ plugins/available/rvm.plugin.bash
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Load RVM, if you are using it
+[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
+# Load the auto-completion script if RVM was loaded.
+if [ "$rvm_path" ]
+then
+    [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
+fi
+
 switch () {
   rvm $1
   local v=$(rvm_version)
