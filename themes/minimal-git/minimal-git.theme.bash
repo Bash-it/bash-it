b36d86ddff8f83c691e761d1f9a9789ca8d52fbd
Mark Szymanski <mrman208@me.com>
2011-06-24 11:36:11 -0500

    Add minimal-git theme
    Just like the minimal theme, but with git info

---
+++ themes/minimal-git/minimal-git.theme.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_PREFIX="${cyan}(${green}"
+SCM_THEME_PROMPT_SUFFIX="${cyan})"
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+
+PS1="$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} "
