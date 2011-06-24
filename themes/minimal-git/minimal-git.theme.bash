989de733309ef79308b3806268ad35467f7ee9b8
Mark Szymanski <mrman208@me.com>
2011-06-24 12:06:30 -0500

    No more bold

--- themes/minimal-git/minimal-git.theme.bash
+++ themes/minimal-git/minimal-git.theme.bash
@@ -3,6 +3,6 @@
 SCM_THEME_PROMPT_PREFIX="${cyan}(${green}"
 SCM_THEME_PROMPT_SUFFIX="${cyan})"
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 
 PS1="$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} "
