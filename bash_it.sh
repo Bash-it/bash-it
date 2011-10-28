e3194237e905f80231873fd6b1b778b11409055c
John Schulz <JFSIII@JFSIII.org>
2011-10-28 19:22:20 -0400

    `$BASH_IT` should be conditionally defined before use.
    7483c6dac351f836bd574c9cdff4a23df7cd507f moved one line too many.

--- bash_it.sh
+++ bash_it.sh
@@ -4,6 +4,9 @@
 # Reload Library
 alias reload='source ~/.bash_profile'
 
+# Only set $BASH_IT if it's not already set
+[ -z "$BASH_IT" ] && export BASH_IT=$HOME/.bash_it
+
 # Load enabled aliases, completion, plugins
 for file_type in "aliases" "completion" "plugins"
 do
@@ -36,7 +39,6 @@ source "${BASH_IT}/themes/colors.theme.bash"
 source "${BASH_IT}/themes/base.theme.bash"
 
 # library
-[ -z "$BASH_IT" ] && export BASH_IT=$HOME/.bash_it
 LIB="${BASH_IT}/lib/*.bash"
 for config_file in $LIB
 do
