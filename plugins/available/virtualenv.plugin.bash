aa622e8c3468aa0b6b910a51ac5de778229a4ca0
Travis Swicegood <development@domain51.com>
2011-11-15 10:47:37 -0600

    add mkvenv shortcut

--- plugins/available/virtualenv.plugin.bash
+++ plugins/available/virtualenv.plugin.bash
@@ -3,3 +3,8 @@
 # make sure virtualenvwrapper is enabled if availalbe
 [[ `which virtualenvwrapper.sh` ]] && . virtualenvwrapper.sh
 
+# create a new virtualenv for this directory
+function mkvenv {
+  cwd=`basename \`pwd\``
+  mkvirtualenv --no-site-packages --distribute $cwd
+}
