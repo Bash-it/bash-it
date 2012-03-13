5c29f7f1ff9e148b9ac51496eee88f23572f844c
Travis Swicegood <development@domain51.com>
2012-03-13 07:23:27 -0700

    add a multi-line doubletime theme

---
+++ themes/doubletime_multiline/doubletime_multiline.theme.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source "$BASH_IT/themes/doubletime/doubletime.theme.bash"
+
+function prompt_setter() {
+  # Save history
+  history -a
+  history -c
+  history -r
+  if [[ -z "$THEME_PROMPT_CLOCK_FORMAT" ]]
+  then
+      clock="\t"
+  else
+      clock=$THEME_PROMPT_CLOCK_FORMAT
+  fi
+  PS1="
+$clock $(scm_char) [$THEME_PROMPT_HOST_COLOR\u@${THEME_PROMPT_HOST}$reset_color] $(virtualenv_prompt)$(ruby_version_prompt)
+\w
+$(doubletime_scm_prompt)$reset_color $ "
+  PS2='> '
+  PS4='+ '
+}
+
+PROMPT_COMMAND=prompt_setter
