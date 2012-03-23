037f7c391eaa37ba7bc515c6031c4d7e60801309
Antono Vasiljev <self@antono.info>
2012-03-24 01:37:36 +0300

    Better emacs aliases
     - e for nowait
     - em for wait
     - E for emacslient as SUDO_EDITOR
     - always sue emacs if there is no emacs server

--- aliases/available/emacs.aliases.bash
+++ aliases/available/emacs.aliases.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 case $OSTYPE in
-  linux*)
-    alias em='emacs'
-    alias e='emacsclient -n'
-    ;;
-  darwin*)
-    alias em="open -a emacs"
-    ;;
+    linux*)
+        alias e='emacsclient -a emacs -n'
+        alias em='emacsclient -a emacs'
+        alias E="SUDO_EDITOR=\"emacsclient -c -a emacs\" sudoedit"
+        ;;
+    darwin*)
+        alias em="open -a emacs"
+        ;;
 esac
