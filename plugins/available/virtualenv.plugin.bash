1372e7d931bae1542386b2e768df406deeedf306
Travis Swicegood <development@domain51.com>
2012-03-26 11:01:18 -0500

    add a few more helpers for virtualenv

--- plugins/available/virtualenv.plugin.bash
+++ plugins/available/virtualenv.plugin.bash
@@ -8,3 +8,12 @@ function mkvenv {
   cwd=`basename \`pwd\``
   mkvirtualenv --no-site-packages --distribute $cwd
 }
+
+# create a new virtualenv for the branch you're currently in
+function mkvbranch {
+  mkvirtualenv --no-site-packages --distribute "$(basename `pwd`)@$(git_prompt_info)"
+}
+
+function wovbranch {
+  workon "$(basename `pwd`)@$(git_prompt_info)"
+}
