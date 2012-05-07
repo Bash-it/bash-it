bae8a1cd6ff981bfa1e49f1b9d210b3492ce0bdf
Mike Thornton <msthornton@gmail.com>
2012-05-07 15:48:38 -0500

    Make it so rbenv doesn't try to run if it's not installed

--- plugins/available/rbenv.plugin.bash
+++ plugins/available/rbenv.plugin.bash
@@ -2,7 +2,7 @@
 
 # Load rbebv, if you are using it
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+[[ `which rbenv` ]] && eval "$(rbenv init -)"
 
 # Load the auto-completion script if rbenv was loaded.
-source ~/.rbenv/completions/rbenv.bash
+[[ -e ~/.rbenv/completions/rbenv.bash ]] && source ~/.rbenv/completions/rbenv.bash
