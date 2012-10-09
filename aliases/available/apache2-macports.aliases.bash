8073340a09e0fab494a240f44949b2e007f6d2e9
Vaidas Zilionis <vaidas@zilionis.net>
2012-10-09 17:14:37 +0300

    osx macports (apache2) aliases

---
+++ aliases/available/apache2-macports.aliases.bash
@@ -0,0 +1,6 @@
+cite 'about-alias'
+about-alias 'macports apache2 aliases'
+
+alias apache2start='sudo /opt/local/etc/LaunchDaemons/org.macports.apache2/apache2.wrapper start'
+alias apache2stop='sudo /opt/local/etc/LaunchDaemons/org.macports.apache2/apache2.wrapper stop'
+alias apache2restart='sudo /opt/local/etc/LaunchDaemons/org.macports.apache2/apache2.wrapper restart'
