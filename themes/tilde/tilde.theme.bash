b6c67544be23c2d34fa7e8adf063b6b0dea1b8fb
Pychimp <pratheek.i@gmail.com>
2012-11-03 22:38:47 +0530

    added a theme (tilde)

---
+++ themes/tilde/tilde.theme.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX="${purple}"
+GIT_THEME_PROMPT_SUFFIX="${purple} "
+
+function echoer() {
+PS1="$(scm_prompt_info)${cyan} \w ${green}→${reset_color} "
+}
+
+PROMPT_COMMAND=echoer; 
