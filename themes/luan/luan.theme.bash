d9a5cbf2ba8340bd26d1be298486a617916608be
Luan Santos <cfcluan@gmail.com>
2012-12-15 00:04:11 -0800

    Added custom theme

---
+++ themes/luan/luan.theme.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX="(${yellow}"
+SCM_THEME_PROMPT_SUFFIX="${normal})"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX="(${yellow}"
+GIT_THEME_PROMPT_SUFFIX="${normal})"
+
+RVM_THEME_PROMPT_PREFIX=""
+RVM_THEME_PROMPT_SUFFIX=""
+
+function prompt_command() {
+    dtime="${yellow}\T${normal}"
+    user_host="${green}\u@${cyan}\h${normal}"
+    current_dir="${bold_blue}\w${normal}"
+    rvm_ruby="${bold_red}$(ruby_version_prompt)${normal}"
+    git_branch="$(scm_prompt_info)${normal}"
+    prompt="${bold_green}\$${normal} "
+    arrow="${bold_white}▶${normal} "
+    prompt="${bold_green}\$${normal} "
+
+    PS1="${dtime} ${user_host}:${current_dir} ${rvm_ruby} ${git_branch}
+      $arrow $prompt"
+}
+
+PROMPT_COMMAND=prompt_command;
