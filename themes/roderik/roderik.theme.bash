a202bf9e5a6fc70b0dd01eb06dd2befbf1c5e79e
Roderik van der Veer <roderik@vanderveer.be>
2012-12-30 15:49:32 +0100

    Added my own theme, based on gentoo's prompt

---
+++ themes/roderik/roderik.theme.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWSTASHSTATE=true
+
+export PROMPT_DIRTRIM=3
+
+function prompt_command() {
+    if [[ ${EUID} == 0 ]] ; then
+        PS1="[\t]${yellow}[${red}\u@\h ${green}\w${yellow}]${red}$(__git_ps1 "(%s)")${normal}\\$ "
+    else
+        PS1="[\t]${yellow}[${cyan}\u@\h ${green}\w${yellow}]${red}$(__git_ps1 "(%s)")${normal}\\$ "
+    fi
+}
+
+PROMPT_COMMAND=prompt_command;
