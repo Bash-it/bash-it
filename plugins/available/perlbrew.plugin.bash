98bf368dd4db1446f90506242f5f1bcf7e47d029
diegok <diego@freekeylabs.com>
2013-01-19 14:03:31 +0100

    perlbrew plugin

---
+++ plugins/available/perlbrew.plugin.bash
@@ -0,0 +1,6 @@
+# Load perlbrew, if you are using it
+
+cite about-plugin
+about-plugin 'load perlbrew, if you are using it'
+
+[[ -e ~/perl5/perlbrew/etc/bashrc ]] && source ~/perl5/perlbrew/etc/bashrc
