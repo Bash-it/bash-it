e0bac1cdc64946c0648108d671c2f4cf7c45f1b2
Darayus Nanavati <darayus.contact@gmail.com>
2013-04-26 03:15:27 +1000

    Fix zork  green
    The zork theme SCM_THEME_PROMPT_CLEAN variable of bold_green did not
    appear green in some terminal themes. Changed to just green which seems
    to look the same or similar

--- themes/zork/zork.theme.bash
+++ themes/zork/zork.theme.bash
@@ -2,7 +2,7 @@ SCM_THEME_PROMPT_PREFIX=""
 SCM_THEME_PROMPT_SUFFIX=""
 
 SCM_THEME_PROMPT_DIRTY=" ${bold_red}✗${normal}"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
+SCM_THEME_PROMPT_CLEAN=" ${green}✓${normal}"
 SCM_GIT_CHAR="${bold_green}±${normal}"
 SCM_SVN_CHAR="${bold_cyan}⑆${normal}"
 SCM_HG_CHAR="${bold_red}☿${normal}"
