7f20ff3f1d2a933f7a04bd6c8d547ca067a23935
Jason Benterou <jason.benterou@gmail.com>
2013-07-24 10:54:16 -0700

    Sublime Text CLI plugin

--- plugins/available/subl.plugin.bash
+++ plugins/available/subl.plugin.bash
@@ -1,4 +1,13 @@
 cite about-plugin
 about-plugin 'Add Sublime Text cli binary "subl" to path'
 
-export PATH=$PATH:/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl
+SUBL_BIN="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+SUBL_LOCAL_DIR=$BASH_IT/.subl-bin/
+SUBL_LOCAL_BIN=$BASH_IT/.subl-bin/subl
+
+if [ ! -f $SUBL_LOCAL_BIN ]; then
+  mkdir -p $SUBL_LOCAL_DIR
+  ln -s "$SUBL_BIN" $SUBL_LOCAL_BIN
+fi
+
+export PATH=$PATH:$SUBL_LOCAL_DIR
