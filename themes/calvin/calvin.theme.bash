8045c4cc155024528752b3ce79009d3dc02c1eba
Calvin Giles <calvin.giles@gmail.com>
2013-08-28 23:52:18 +0100

    Removed scm_info

--- themes/calvin/calvin.theme.bash
+++ themes/calvin/calvin.theme.bash
@@ -15,7 +15,7 @@ RVM_THEME_PROMPT_SUFFIX="|"
 
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="${yellow}$(ruby_version_prompt) ${purple}\u@\h${reset_color}:\W${bold_cyan}${green}$(scm_prompt_info)${reset_color}${reset_color}\$ "
+    PS1="${yellow}$(ruby_version_prompt) ${purple}\u@\h${reset_color}:\W${bold_cyan}${green}${reset_color}${reset_color}\$ "
 }
 
 PROMPT_COMMAND=prompt_command;
