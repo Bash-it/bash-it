c3a061599fdce0db5b793f29cb84cfda6ecddddc
Calvin Giles <calvin.giles@gmail.com>
2013-08-29 00:16:29 +0100

    Fixed weird color bug that happens when git is clean.

--- themes/calvin/calvin.theme.bash
+++ themes/calvin/calvin.theme.bash
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-SCM_NONE_CHAR=''
-SCM_THEME_PROMPT_DIRTY=" ${red}✗${reset_color}"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${reset_color}"
-SCM_THEME_PROMPT_PREFIX=" ${reset_color}|$(scm_char) "
-SCM_THEME_PROMPT_SUFFIX="${reset_color}|${reset_color}"
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-GIT_THEME_PROMPT_PREFIX=" ${reset_color}|$(scm_char) "
-GIT_THEME_PROMPT_SUFFIX="${reset_color}|${reset_color}"
+GIT_THEME_PROMPT_PREFIX=" ${green}|$(scm_char) "
+GIT_THEME_PROMPT_SUFFIX="${green}|"
 
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="${yellow}$(ruby_version_prompt) ${purple}\u@\h${reset_color}:\W${bold_cyan}${green}$(scm_prompt_info)${reset_color}${reset_color}\$ "
+    PS1="${yellow}$(ruby_version_prompt) ${purple}\u@\h${reset_color}:\W${green}${bold_cyan}${green}$(scm_prompt_info) ${green}${reset_color}\$ "
 }
 
 PROMPT_COMMAND=prompt_command;
