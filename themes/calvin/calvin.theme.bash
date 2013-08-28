d1c62cacf4a74005f23d5fe33bf8292904906fe6
Calvin Giles <calvin.giles@gmail.com>
2013-08-28 23:46:47 +0100

    Added calvin theme

---
+++ themes/calvin/calvin.theme.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+SCM_NONE_CHAR=''
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" ${reset_color}|$(scm_char) "
+SCM_THEME_PROMPT_SUFFIX="${reset_color}|"
+
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+function prompt_command() {
+    #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
+    PS1="${yellow}$(ruby_version_prompt) ${purple}\u@\h${reset_color}:\W${bold_cyan}${green}$(scm_prompt_info)${reset_color}\$ "
+}
+
+PROMPT_COMMAND=prompt_command;
