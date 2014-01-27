ebb9216e51903ac9a51989c4514ed8d0dbf69c42
Uri Sharf <uri.sharf@gmail.com>
2014-01-27 11:57:26 +0100

    make dependency diagram use tempfiles

--- aliases/available/docker.aliases.bash
+++ aliases/available/docker.aliases.bash
@@ -8,7 +8,19 @@ alias dkpsa='docker ps -a'
 alias dki='docker images'
 alias dkrm='docker rm $(docker ps -a -q)'  # delete all stopped containers
 alias dkrmi='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
-alias dkideps='docker images -viz | dot -Tpng -o docker.png && xdg-open docker.png ' # with graphviz
+
+function dkideps() {
+    OUT=$(mktemp -t docker-viz-XXXX.png)
+    docker images -viz | dot -Tpng > $OUT
+    case $OSTYPE in
+        linux*)
+            xdg-open $OUT
+            ;;
+        darwin*)
+            open $OUT
+            ;;
+    esac
+}
 
 # docker run [container] env
 function dkre() {
