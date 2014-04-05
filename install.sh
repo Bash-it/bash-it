2f4ca5ed069f620bb4e8d55069ce11c2e58842a9
John A. Barbuto <jbarbuto@gmail.com>
2014-04-05 15:28:27 -0700

    Use relative symlinks during installation
    When using dotfiles synced between different machines, this allows
    enabled aliases, completions, and plugins to work with varying
    home directories.

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ function load_all() {
       [ ${filename:0:1} = "_" ] && continue
       dest="${BASH_IT}/${file_type}/enabled/${filename}"
       if [ ! -e "${dest}" ]; then
-          ln -s "${src}" "${dest}"
+          ln -s "../available/${filename}" "${dest}"
       else
           echo "File ${dest} exists, skipping"
       fi
@@ -57,7 +57,7 @@ function load_some() {
         read -p "Would you like to enable the ${file_name%%.*} $file_type? [Y/N] " RESP
         case $RESP in
         [yY])
-          ln -s "$path" "$BASH_IT/$file_type/enabled"
+          ln -s "../available/${file_name}" "$BASH_IT/$file_type/enabled"
           break
           ;;
         [nN])
