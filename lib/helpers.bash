f14aab74a339201633bf248836e389d79a32b078
John A. Barbuto <jbarbuto@gmail.com>
2014-04-06 13:27:16 -0700

    Use relative symlinks for enabling plugins
    This extends 10bda0c for post-install enablements as well.

--- lib/helpers.bash
+++ lib/helpers.bash
@@ -261,7 +261,7 @@ _enable-thing ()
         do
             plugin=$(basename $f)
             if [ ! -h $BASH_IT/$subdirectory/enabled/$plugin ]; then
-                ln -s $BASH_IT/$subdirectory/available/$plugin $BASH_IT/$subdirectory/enabled/$plugin
+                ln -s ../available/$plugin $BASH_IT/$subdirectory/enabled/$plugin
             fi
         done
     else
@@ -279,7 +279,7 @@ _enable-thing ()
 
         mkdir -p $BASH_IT/$subdirectory/enabled
 
-        ln -s $BASH_IT/$subdirectory/available/$plugin $BASH_IT/$subdirectory/enabled/$plugin
+        ln -s ../available/$plugin $BASH_IT/$subdirectory/enabled/$plugin
     fi
 
     printf '%s\n' "$file_entity enabled."
