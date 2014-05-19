0c84bf9cb92f8aaa1140419d7195740a39177bef
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-05-19 10:33:05 -0500

    adjust prompt

--- themes/neato/neato.theme.bash
+++ themes/neato/neato.theme.bash
@@ -12,7 +12,7 @@ GIT_THEME_PROMPT_SUFFIX="${green}]"
 
 function prompt_command() {
     # PS1="${green}\u@\h ${blue}\T ${reset_color}${white}\w${reset_color}$(scm_prompt_info)${blue} →${bold_blue} ${reset_color} ";
-    PS1="${bold_green}\u@\h ${bold_blue}[\T] ${reset_color}${white}[\w]${reset_color}$(scm_prompt_info)${blue}\n-> ${bold_blue}% ${reset_color}";
+    PS1="${bold_green}\u@\h ${bold_blue}[\T] ${reset_color}${white}[\w]${reset_color}$(scm_prompt_info)${reset_color}\n${blue}-> ${bold_blue}% ${reset_color}";
 }
 
 PROMPT_COMMAND=prompt_command;
