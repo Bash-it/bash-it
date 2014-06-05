9f6ae19960fa7f8665768fa4f27043ee947bcabb
philipjohn <phil@philipjohn.co.uk>
2014-06-05 09:29:14 +0100

    Create svn.aliases.bash

---
+++ aliases/available/svn.aliases.bash
@@ -0,0 +1,15 @@
+cite 'about-alias'
+about-alias 'common svn abbreviations'
+
+# Aliases
+alias svs='svn status'
+alias sa='svn add'
+alias sci='svn ci -m'
+alias sco='svn co'
+alias sup='svn up'
+alias scu='svn cleanup'
+alias sli='svn list'
+alias sdel='svn delete'
+alias sdif='svn diff'
+alias slog='svn log'
+alias smv='svn move'
