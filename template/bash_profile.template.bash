c768d8e199039f84be6392ac5e4b9490e880cb86
Maxim Kovgan <maxk@fortscale.com>
2014-07-11 11:05:17 +0300

    setting default editor to /usr/bin/vim. sorry mac users

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -17,8 +17,8 @@ export BASH_IT_THEME='bobby'
 export GIT_HOSTING='git@git.domain.com'
 
 # Set my editor and git editor
-export EDITOR="/usr/bin/mate -w"
-export GIT_EDITOR='/usr/bin/mate -w'
+export EDITOR='/usr/bin/vim'
+export GIT_EDITOR='/usr/bin/vim'
 
 # Set the path nginx
 export NGINX_PATH='/opt/nginx'
