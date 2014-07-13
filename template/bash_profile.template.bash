a32e0e3cd10aed2173281cd60700aaad2ab3569a
Maxim Kovgan <maxk@fortscale.com>
2014-07-13 08:31:56 +0300

    use sexy theme by default

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -11,7 +11,7 @@ export BASH_IT=$HOME/.bash_it
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
-export BASH_IT_THEME='bobby'
+export BASH_IT_THEME='sexy'
 
 # Your place for hosting Git repos. I use this for private repos.
 export GIT_HOSTING='git@git.domain.com'
