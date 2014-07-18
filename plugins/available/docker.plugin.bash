84b1dd40fb8a742bc9d76e350706a2b31474731d
Travis Swicegood <development@domain51.com>
2014-07-18 17:04:20 -0500

    Add docker-enter

--- plugins/available/docker.plugin.bash
+++ plugins/available/docker.plugin.bash
@@ -5,4 +5,9 @@ about-plugin 'Helpers to get Docker setup correctly for boot2docker'
 # of boot2docker, or its configured for a different IP
 if [[ `uname -s` == "Darwin" ]]; then
   export DOCKER_HOST=tcp://192.168.59.103:2375
+
+  docker-enter() {
+    boot2docker ssh '[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter'
+    boot2docker ssh -t sudo "/var/lib/boot2docker/docker-enter \"$1\""
+  }
 fi
