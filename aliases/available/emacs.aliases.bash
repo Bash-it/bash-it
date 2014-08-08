d4c340b008c8f13bedd5c80690d4810758d10ff7
Hossein Sekhavat <sekhavat17@gmail.com>
2014-08-08 18:55:48 +0430

    Remove redundant arguments and replace 'sudoedit' with 'sudo -e' to be Mac-compatible

--- aliases/available/emacs.aliases.bash
+++ aliases/available/emacs.aliases.bash
@@ -5,7 +5,7 @@ case $OSTYPE in
   linux*)
     alias em='emacs'
     alias e='emacsclient -n'
-    alias E="SUDO_EDITOR=\"emacsclient -c -a emacs\" sudoedit"
+    alias E='SUDO_EDITOR="emacsclient" sudo -e'
     ;;
   darwin*)
     alias em='open -a emacs'
