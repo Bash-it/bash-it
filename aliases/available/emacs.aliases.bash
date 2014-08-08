a19e1cdb4b755c10830d936b7924e0ad71d9d617
Hossein Sekhavat <sekhavat17@gmail.com>
2014-08-08 18:20:43 +0430

    Add alias 'E' for sudo-editing with emacsclient
    More description available at http://emacs-fu.blogspot.com/2011/12/system-administration-with-emacs.html

--- aliases/available/emacs.aliases.bash
+++ aliases/available/emacs.aliases.bash
@@ -5,6 +5,7 @@ case $OSTYPE in
   linux*)
     alias em='emacs'
     alias e='emacsclient -n'
+    alias E="SUDO_EDITOR=\"emacsclient -c -a emacs\" sudoedit"
     ;;
   darwin*)
     alias em='open -a emacs'
