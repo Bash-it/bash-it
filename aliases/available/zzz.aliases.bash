34cc8a5e1d062410ab7f5355b046f216b68cc22e
Michael Turquette <mturquette@deferred.io>
2014-08-31 19:49:41 -0700

    aliases: zzz: update linux kernel build helpers
    TODO: zzz.aliases.bash should probably live in the custom/ directory
    
    Signed-off-by: Michael Turquette <mturquette@deferred.io>

--- aliases/available/zzz.aliases.bash
+++ aliases/available/zzz.aliases.bash
@@ -1,6 +1,11 @@
 cite about-alias
 about-alias 'development aliases'
 
-alias m='LOADADDR=0x80800000 make -j16 uImage dtbs'
+alias m='LOADADDR=0x80800000 make -j16 zImage dtbs'
 alias m2='m 2> errors.err'
-alias c='cp arch/arm/boot/uImage arch/arm/boot/dts/*.dtb /media/mturquette/165A-3C60/ && sync'
+#alias c='cp arch/arm/boot/uImage arch/arm/boot/dts/omap4-panda-es.dtb /media/mturquette/boot/ && sync'
+alias c='cp arch/arm/boot/zImage arch/arm/boot/dts/omap4-panda-es.dtb /var/lib/tftpboot/ && sync'
+#alias t='cp arch/arm/boot/uImage arch/arm/boot/dts/omap4-panda-es.dtb /var/lib/tftpboot/ && sync'
+
+# function p() defined in ~/.bash_it/custom/zzz.bash
+alias pp='p 1'
