58b52819ab217ab3715a848f297f09c562acbd46
Travis Swicegood <development@domain51.com>
2014-09-04 13:10:35 -0700

    Add a few new docker aliases

--- plugins/available/docker.plugin.bash
+++ plugins/available/docker.plugin.bash
@@ -11,3 +11,15 @@ if [[ `uname -s` == "Darwin" ]]; then
     boot2docker ssh -t sudo "/var/lib/boot2docker/docker-enter \"$1\""
   }
 fi
+
+function docker-remove-most-recent-container() {
+  about 'attempt to remove the most recent container from docker ps -a'
+  group 'docker'
+  docker ps -a | head -2 | tail -1 | awk '{print $NF}' | xargs docker rm
+}
+
+function docker-remove-most-recent-image() {
+  about 'attempt to remove the most recent image from docker images'
+  group 'docker'
+  docker images | head -2 | tail -1 | awk '{print $3}' | xargs docker rmi
+}
