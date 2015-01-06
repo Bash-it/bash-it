0c70325542296097307599883ffdaec1dca1ecfa
Matt Wismer <mattwis86@gmail.com>
2015-01-06 16:50:44 -0500

    Use basename instead of sed

--- lib/preview.bash
+++ lib/preview.bash
@@ -10,7 +10,7 @@ then
   THEMES="$BASH_IT/themes/*/*.theme.bash"
   for theme in $THEMES
   do
-    BASH_IT_THEME=$(echo $theme | sed "s/\//\n/g" | tail -1 | sed "s/.theme.bash//")
+    BASH_IT_THEME=$(basename -s '.theme.bash' $theme)
     echo "
     $BASH_IT_THEME"
     echo "" | bash --init-file $BASH_IT/bash_it.sh -i
