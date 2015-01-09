f23cc41f004be14eff1b3c84d47b27f67b45d89d
Travis Swicegood <development@domain51.com>
2015-01-09 11:40:05 -0800

    Add in a multi-line powerline theme

---
+++ themes/powerline-multiline/powerline-multiline.theme.bash
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+THEME_PROMPT_SEPARATOR=""
+
+SHELL_SSH_CHAR=" "
+SHELL_THEME_PROMPT_COLOR=32
+SHELL_SSH_THEME_PROMPT_COLOR=208
+
+VIRTUALENV_CHAR="ⓔ "
+VIRTUALENV_THEME_PROMPT_COLOR=35
+
+SCM_NONE_CHAR=""
+SCM_GIT_CHAR=" "
+
+SCM_THEME_PROMPT_CLEAN=""
+SCM_THEME_PROMPT_DIRTY=""
+
+SCM_THEME_PROMPT_COLOR=240
+SCM_THEME_PROMPT_CLEAN_COLOR=231
+SCM_THEME_PROMPT_DIRTY_COLOR=196
+SCM_THEME_PROMPT_STAGED_COLOR=220
+SCM_THEME_PROMPT_UNSTAGED_COLOR=166
+
+CWD_THEME_PROMPT_COLOR=238
+
+LAST_STATUS_THEME_PROMPT_COLOR=52
+
+PROMPT_LENGTH=0
+
+function set_rgb_color {
+    if [[ "${1}" != "-" ]]; then
+        fg="38;5;${1}"
+    fi
+    if [[ "${2}" != "-" ]]; then
+        bg="48;5;${2}"
+        [[ -n "${fg}" ]] && bg=";${bg}"
+    fi
+    echo -e "\[\033[${fg}${bg}m\]"
+}
+
+function powerline_shell_prompt {
+    if [[ -n "${SSH_CLIENT}" ]]; then
+        SHELL_PROMPT="${bold_white}$(set_rgb_color - ${SHELL_SSH_THEME_PROMPT_COLOR}) ${SHELL_SSH_CHAR}\u@\h ${normal}"
+        LAST_THEME_COLOR=${SHELL_SSH_THEME_PROMPT_COLOR}
+        PROMPT_LENGTH=$(($PROMPT_LENGTH + ${#USER} + 1 + ${#HOST} + 2))
+    else
+        SHELL_PROMPT="${bold_white}$(set_rgb_color - ${SHELL_THEME_PROMPT_COLOR}) \u ${normal}"
+        LAST_THEME_COLOR=${SHELL_THEME_PROMPT_COLOR}
+        PROMPT_LENGTH=$(($PROMPT_LENGTH + ${#USER} + 2))
+    fi
+}
+
+function powerline_virtualenv_prompt {
+    local environ=""
+
+    if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
+        environ="conda: $CONDA_DEFAULT_ENV"
+    elif [[ -n "$VIRTUAL_ENV" ]]; then
+        environ=$(basename "$VIRTUAL_ENV")
+    fi
+
+    if [[ -n "$environ" ]]; then
+        VIRTUALENV_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${VIRTUALENV_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}$(set_rgb_color - ${VIRTUALENV_THEME_PROMPT_COLOR}) ${VIRTUALENV_CHAR}$environ ${normal}"
+        LAST_THEME_COLOR=${VIRTUALENV_THEME_PROMPT_COLOR}
+        PROMPT_LENGTH=$(($PROMPT_LENGTH + 2 + 2 + ${#environ} + 1))
+    else
+        VIRTUALENV_PROMPT=""
+    fi
+}
+
+function powerline_scm_prompt {
+    scm_prompt_vars
+
+    if [[ "${SCM_NONE_CHAR}" != "${SCM_CHAR}" ]]; then
+        if [[ "${SCM_DIRTY}" -eq 3 ]]; then
+            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_STAGED_COLOR} ${SCM_THEME_PROMPT_COLOR})"
+        elif [[ "${SCM_DIRTY}" -eq 2 ]]; then
+            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_UNSTAGED_COLOR} ${SCM_THEME_PROMPT_COLOR})"
+        elif [[ "${SCM_DIRTY}" -eq 1 ]]; then
+            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_DIRTY_COLOR} ${SCM_THEME_PROMPT_COLOR})"
+        else
+            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_CLEAN_COLOR} ${SCM_THEME_PROMPT_COLOR})"
+        fi
+        if [[ "${SCM_GIT_CHAR}" == "${SCM_CHAR}" ]]; then
+            SCM_PROMPT+=" ${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
+        fi
+        SCM_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${SCM_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}${SCM_PROMPT} ${normal}"
+        LAST_THEME_COLOR=${SCM_THEME_PROMPT_COLOR}
+    else
+        SCM_PROMPT=""
+    fi
+}
+
+function powerline_cwd_prompt {
+    CWD_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${CWD_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}$(set_rgb_color - ${CWD_THEME_PROMPT_COLOR}) \w ${normal}$(set_rgb_color ${CWD_THEME_PROMPT_COLOR} -)${normal}"
+    LAST_THEME_COLOR=${CWD_THEME_PROMPT_COLOR}
+    mypwd="${PWD/#$HOME/~/}"
+    PROMPT_LENGTH=$(($PROMPT_LENGTH + ${#mypwd} + 2))
+}
+
+function powerline_last_status_prompt {
+    if [[ "$1" -eq 0 ]]; then
+        LAST_STATUS_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} -)${THEME_PROMPT_SEPARATOR}${normal}"
+    else
+        LAST_STATUS_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${LAST_STATUS_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}$(set_rgb_color - ${LAST_STATUS_THEME_PROMPT_COLOR}) ${LAST_STATUS} ${normal}$(set_rgb_color ${LAST_STATUS_THEME_PROMPT_COLOR} -)${THEME_PROMPT_SEPARATOR}${normal}"
+    fi
+}
+
+function powerline_prompt_command() {
+    local LAST_STATUS="$?"
+    PROMPT_LENGTH=0
+
+    powerline_shell_prompt
+    powerline_virtualenv_prompt
+    powerline_cwd_prompt
+    powerline_scm_prompt
+    powerline_last_status_prompt LAST_STATUS
+
+    PADDING=$(($(tput cols) - $PROMPT_LENGTH))
+
+    FIRST_LINE="${SHELL_PROMPT}${VIRTUALENV_PROMPT}${CWD_PROMPT}"
+    for i in $(seq 1 $PADDING); do FIRST_LINE="${FIRST_LINE}$(set_rgb_color - ${CWD_THEME_PROMPT_COLOR}) "; done
+
+    PS1="${FIRST_LINE}${normal}\n${SCM_PROMPT}${LAST_STATUS_PROMPT} "
+}
+
+PROMPT_COMMAND=powerline_prompt_command
+
