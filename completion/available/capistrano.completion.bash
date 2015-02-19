2195d3aa76638d6d7be35f6cb3897be920c8f170
Timo Webler <timo.webler@dkd.de>
2015-02-19 13:26:23 +0100

    Add the compatibility for capistrano gem version 3
    Use all tasks for completion

--- completion/available/capistrano.completion.bash
+++ completion/available/capistrano.completion.bash
@@ -7,7 +7,14 @@ _capcomplete() {
     if [ -f Capfile ]; then
         recent=`ls -t .cap_tasks~ Capfile **/*.cap 2> /dev/null | head -n 1`
         if [[ $recent != '.cap_tasks~' ]]; then
-            cap --tool --tasks | cut -d " " -f 2 > .cap_tasks~
+            cap --version | grep 'Capistrano v2.' > /dev/null
+            if [ $? -eq 0 ]; then
+              # Capistrano 2.x
+              cap --tool --verbose --tasks | cut -d " " -f 2 > .cap_tasks~
+            else
+              # Capistrano 3.x
+              cap --all --tasks | cut -d " " -f 2 > .cap_tasks~
+            fi
         fi
         COMPREPLY=($(compgen -W "`cat .cap_tasks~`" -- ${COMP_WORDS[COMP_CWORD]}))
         return 0
