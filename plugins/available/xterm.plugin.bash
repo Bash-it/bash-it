66fd6411a0eab67de1080db1cabab757ada80a54
James Ratcliffe <james.ratcliffe@James-Rs-Macbook-Air.local>
2015-05-10 15:17:45 -0400

    Fix for issue #108 that restores xterm plugin functionality on OS X.

--- plugins/available/xterm.plugin.bash
+++ plugins/available/xterm.plugin.bash
@@ -1,17 +1,9 @@
-# This plugin is known to cause issues on OS X with the evaluation of
-# colors.  Please read [issue 108] for more information.
-#
-# You can manually turn this on by symlinking it into your
-# plugins/enabled/ directory.
-#
-# [issue 108]: https://github.com/revans/bash-it/issues/108
-
 cite about-plugin
 about-plugin 'automatically set your xterm title with host and location info'
 
 set_xterm_title () {
     local title="$1"
-    echo -ne "\e]0;$title\007"
+    echo -ne "\033]0;$title\007"
 }
 
 
