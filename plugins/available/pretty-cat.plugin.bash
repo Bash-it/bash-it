af2f0304cbf8fe22ad3e29b1445879981e27fd19
Miguel Morales <mimoralea@gmail.com>
2015-05-12 15:57:39 -0500

    Add proper exit code

--- plugins/available/pretty-cat.plugin.bash
+++ plugins/available/pretty-cat.plugin.bash
@@ -1,21 +1,21 @@
 cite about-plugin                                                                                        
 about-plugin 'pygmentize instead of cat to terminal if possible'                                         
 
-if [ -z $(which pygmentize) ]                                                                            
-then                                                                                                     
-    echo "Pygments is required to use this pluging"                                                      
-    echo "Install it by doing 'pip install Pygments' as the superuser"                                   
-    exit                                                                                                 
-fi                                                                                                       
+if [ -z $(which pygmentize) ]
+then
+    echo "Pygments is required to use this pluging"
+    echo "Install it by doing 'pip install Pygments' as the superuser"
+    exit 1
+fi
 
-# get the actual cat binary                                                                              
-CAT_BIN=$(which cat)                                                                                     
+# get the actual cat binary
+CAT_BIN=$(which cat)
 
-# replace the cat binary for a pygmentize if possible                                                    
-cat()                                                                                                    
-{                                                                                                        
-    for var;                                                                                             
-    do                                                                                                   
-        pygmentize "$var" 2>/dev/null || "$CAT_BIN" "$var";                                              
-    done                                                                                                 
+# replace the cat binary for a pygmentize if possible
+cat()
+{
+    for var;
+    do
+        pygmentize "$var" 2>/dev/null || "$CAT_BIN" "$var";
+    done
 }
