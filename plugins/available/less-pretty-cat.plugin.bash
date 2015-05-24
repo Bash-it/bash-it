6821b6c0dcc94f5b9a4c50ea0ef2db117bc9f8be
Miguel Morales <mimoralea@gmail.com>
2015-05-24 18:04:42 -0500

    Add less and remove hard exit

--- plugins/available/less-pretty-cat.plugin.bash
+++ plugins/available/less-pretty-cat.plugin.bash
@@ -5,20 +5,28 @@ if [ -z $(which pygmentize) ]
 then
     echo "Pygments is required to use this plugin"
     echo "Install it by doing 'pip install Pygments' as the superuser"
-    exit 1
 fi
 
-# get the actual cat binary
+# get the full paths to binaries
 CAT_BIN=$(which cat)
+LESS_BIN=$(which less)
 
-# replace the cat binary for a pygmentize if possible
+# pigmentize cat and less outputs
 cat()
 {
     about 'runs either pygmentize or cat on each file passed in'
-    param '*: files to concatenate (as normally passed to cat'
+    param '*: files to concatenate (as normally passed to cat)'
     example 'cat mysite/manage.py dir/text-file.txt'
     for var;
     do
         pygmentize "$var" 2>/dev/null || "$CAT_BIN" "$var";
     done
+}
+
+less()
+{
+    about 'it pigments the file passed in and passes it to less for pagination'
+    param '$1: the file to paginate with less'
+    example 'less mysite/manage.py'
+    pygmentize "$1" | "$LESS_BIN" -R
 }
