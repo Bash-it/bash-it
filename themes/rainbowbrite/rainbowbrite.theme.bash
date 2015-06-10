1fc585dac48084b55f874c7d62de543a92e0ca7e
jimwhimpey <jimwhimpey@me.com>
2015-06-09 21:35:21 -0700

    More theme updates

--- themes/rainbowbrite/rainbowbrite.theme.bash
+++ themes/rainbowbrite/rainbowbrite.theme.bash
@@ -13,7 +13,7 @@ prompt_setter() {
   # displays user@server in purple
   # PS1="$red$(scm_char) $purple\u@\h$reset_color:$blue\w$yellow$(scm_prompt_info)$(ruby_version_prompt) $black\$$reset_color "
   # no user@server
-  PS1="\n$green\w$yellow$(scm_prompt_info) ${bold_yellow}⚡ $normal "
+  PS1="\n${purple}\w$white$(scm_prompt_info) ${white}➡️  $normal "
   PS2='> '
   PS4='+ '
 }
@@ -24,6 +24,6 @@ SCM_NONE_CHAR='·'
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 SCM_THEME_PROMPT_PREFIX=" ("
-SCM_THEME_PROMPT_SUFFIX="${yellow})"
+SCM_THEME_PROMPT_SUFFIX="${white})"
 RVM_THEME_PROMPT_PREFIX=" ("
 RVM_THEME_PROMPT_SUFFIX=")"
