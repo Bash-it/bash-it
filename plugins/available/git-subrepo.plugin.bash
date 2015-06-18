51872d7beed5a0cbb73665a6a6651a7d6b332a06
Josh Hagins <hagins.josh@gmail.com>
2015-06-18 11:51:51 -0400

    Add git-subrepo plugin

---
+++ plugins/available/git-subrepo.plugin.bash
@@ -0,0 +1,6 @@
+# Load git-subrepo if you are using it, and initialize completions
+
+cite about-plugin
+about-plugin 'load git-subrepo if you are using it, and initialize completions'
+
+[[ -e "${GIT_SUBREPO_ROOT:=~/.git-subrepo}/init" ]] && source "$GIT_SUBREPO_ROOT/init"
