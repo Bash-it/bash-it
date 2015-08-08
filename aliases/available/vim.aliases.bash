6858809e2037dc6dd8a85b73db4c093c00c5dd06
sveneh <sven@digica.re>
2015-08-08 23:53:42 +0200

    improved version of open file in new tab

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -7,11 +7,13 @@ MVIM=$(command -v mvim)
 
 [[ -n $VIM ]] && alias v=$VIM
 
+# open vim in new tab is taken from
+# http://stackoverflow.com/questions/936501/let-gvim-always-run-a-single-instancek
 case $OSTYPE in
   darwin*)
-    [[ -n $MVIM ]] && alias mvimt="mvim --remote-tab"
+	[[ -n $MVIM ]] && function mvimt { command mvim --remote-tab-silent "$@" || command mvim "$@"; }
     ;;
   *)
-    [[ -n $GVIM ]] && alias gvimt="gvim --remote-tab"
+    [[ -n $GVIM ]] && function gvimt { command gvim --remote-tab-silent "$@" || command gvim "$@"; }
     ;;
 esac
