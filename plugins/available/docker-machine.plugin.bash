412bb7fc88d3ebf39571a7f0e20609ef2c8a6375
Lyle Franklin <lfranklin@pivotal.io>
2015-09-09 09:38:07 -0700

    Replace `~` with $HOME in docker-machine plugin
    The `~` caused docker commands to error with the following:
    ```
    $ docker images
    Could not read CA certificate "~/.docker/machine/machines/dev/ca.pem"
    ```
    Switching to $HOME fixes the issue

--- plugins/available/docker-machine.plugin.bash
+++ plugins/available/docker-machine.plugin.bash
@@ -5,7 +5,7 @@ about-plugin 'Helpers to get Docker setup correctly for docker-machine'
 # or its configured for a different IP
 if [[ `uname -s` == "Darwin" ]]; then
   export DOCKER_HOST="tcp://192.168.99.100:2376"
-  export DOCKER_CERT_PATH="~/.docker/machine/machines/dev"
+  export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/dev"
   export DOCKER_TLS_VERIFY=1
   export DOCKER_MACHINE_NAME="dev"
 fi
