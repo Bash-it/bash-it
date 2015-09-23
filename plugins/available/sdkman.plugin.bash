e2a596b7080abff0489a487145b811e03709bdba
Frank Becker <frank@calmdevelopment.de>
2015-09-23 12:30:13 +0200

    renamed gvmtool to sdkman

---
+++ plugins/available/sdkman.plugin.bash
@@ -0,0 +1,5 @@
+cite about-plugin
+about-plugin 'Load Software Development Kit Manager'
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
