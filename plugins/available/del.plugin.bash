f59ab436e4042a16d1a32986b20f018cc1365feb
Miguel Morales <mimoralea@gmail.com>
2015-10-07 15:57:09 -0500

    Delete del.plugin.bash

--- plugins/available/del.plugin.bash
+++
@@ -1,7 +0,0 @@
-cite about-plugin
-about-plugin 'soft delete by moving contents into a hidden folder in tmp'
-
-# the tmp folder gets cleared on reboot
-# so this would actually remove the file on
-# shutdown
-function del() { mkdir -p /tmp/.trash && mv "$@" /tmp/.trash; }
