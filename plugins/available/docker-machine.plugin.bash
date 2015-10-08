e704bd72f3cd9ff5b4fcae533cd71b0118eb48ed
Shash Reddy <sreddy@pivotal.io>
2015-10-08 11:11:12 -0700

    Check whether docker-machine dev is running before configuring env variables
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- plugins/available/docker-machine.plugin.bash
+++ plugins/available/docker-machine.plugin.bash
@@ -1,8 +1,11 @@
 cite about-plugin
 about-plugin 'Helpers to get Docker setup correctly for docker-machine'
 
-# Note, this might need to be different if you use a machine other than 'dev',
-# or its configured for a different IP
+# Note, this might need to be different if you use a machine other than 'dev'
 if [[ `uname -s` == "Darwin" ]]; then
-  eval "$(docker-machine env dev)"
+  # check if dev machine is running
+  docker-machine ls | grep --quiet 'dev.*Running'
+  if [[ "$?" = "0" ]]; then
+    eval "$(docker-machine env dev)"
+  fi
 fi
