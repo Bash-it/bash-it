5b87b2db0e830035f978e895433dfd1a65d33530
Manoj <manojlds@gmail.com>
2015-11-03 12:10:02 +0530

    Using pathmunge in jenv plugin

--- plugins/available/jenv.plugin.bash
+++ plugins/available/jenv.plugin.bash
@@ -2,7 +2,8 @@ cite about-plugin
 about-plugin 'load jenv, if you are using it'
 
 export JENV_ROOT="$HOME/.jenv"
-export PATH="$JENV_ROOT/bin:$PATH"
+pathmunge "$JENV_ROOT/bin"
+
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 
 [[ -e $JENV_ROOT/completions/jenv.bash ]] && source $JENV_ROOT/completions/jenv.bash
