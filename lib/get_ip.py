423da31d2eef7b1f4fc6c0f5e7834606daa24c1c
sherabi <shezaan.topiwala@capitalone.com>
2015-11-13 17:23:13 -0500

    New theme with new PS1 prompt

---
+++ lib/get_ip.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import socket
+ip = socket.gethostbyname(socket.gethostname())
+print(ip)
---
+++ themes/shezaan/shezaan.theme.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+#myip=`ifconfig en4 | egrep "inet " | awk -F" " '{ print $2 }' | sed 's/\/[0-9]*//g'`
+myip=`$BASH_IT/lib/get_ip.py`
+
+function prompt_command() {
+	PS1="${bold_cyan}\u${bold_red}@${bold_green}\H ${bold_yellow}($myip) ${bold_red}[\w] ${reset_color}\d \T\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+}
+
+PROMPT_COMMAND=prompt_command;
