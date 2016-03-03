ca889da3e7e6d82e6efd066ad0314683f215e437
Nils Winkler <nils@nilswinkler.com>
2016-03-03 08:59:11 +0100

    Removed embedded todo script
    Fixes #710

--- plugins/available/todo.plugin.bash
+++ plugins/available/todo.plugin.bash
@@ -1,21 +1,12 @@
 #!/bin/bash
+cite about-plugin
+about-plugin 'Todo.txt integration'
 
 # you may override any of the exported variables below in your .bash_profile
 
-if [ -z "$TODO_DIR" ]; then
-    export TODO_DIR=$BASH_IT/custom  # store todo items in user's custom dir, ignored by git
-fi
 if [ -z "$TODOTXT_DEFAULT_ACTION" ]; then
-    export TODOTXT_DEFAULT_ACTION=ls       # typing 't' by itself will list current todos
-fi
-if [ -z "$TODO_SRC_DIR" ]; then
-    export TODO_SRC_DIR=$BASH_IT/plugins/available/todo
+  # typing 't' by itself will list current todos
+  export TODOTXT_DEFAULT_ACTION=ls
 fi
 
-# respect ENV var set in .bash_profile, default is 't'
-alias $TODO='$TODO_SRC_DIR/todo.sh -d $TODO_SRC_DIR/todo.cfg'
-
-pathmunge $TODO_SRC_DIR after
-
-source $TODO_SRC_DIR/todo_completion   # bash completion for todo.sh
-complete -F _todo $TODO                # enable completion for 't' alias
+alias t='todo.sh'
