d8a22e6a0efbd954c532875fa0c1a108079e211f
AlfredoBejarano <alfredocbejarano@gmail.com>
2016-04-20 00:07:18 -0500

    Adds cooperkid theme by AlfredoBejarano

---
+++ themes/cooperkid/cooperkid.theme.bash
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------#
+#          FILE: cooperkid.zsh-theme                                #
+#            BY: Alfredo Bejarano                                   #
+#      BASED ON: Mr Briggs by Matt Brigg (matt@mattbriggs.net)      #
+# ------------------------------------------------------------------#
+
+SCM_THEME_PROMPT_DIRTY="${yellow}⚡${reset_color}"
+SCM_THEME_PROMPT_AHEAD="${red}!${reset_color}"
+SCM_THEME_PROMPT_CLEAN="${green}✓${reset_color}"
+SCM_THEME_PROMPT_PREFIX=" "
+SCM_THEME_PROMPT_SUFFIX=""
+GIT_SHA_PREFIX="${blue}"
+GIT_SHA_SUFFIX="${reset_color}"
+
+function git_short_sha() {
+  SHA=$(git rev-parse --short HEAD 2> /dev/null) && echo "$GIT_SHA_PREFIX$SHA$GIT_SHA_SUFFIX"
+}
+
+function prompt() {
+    local return_status=""
+    local ruby="${red}$(ruby_version_prompt)${reset_color}"
+    local user_host="${green}\h @ \w${reset_color}"
+    local git_branch="$(git_short_sha)$(scm_prompt_info)"
+    local prompt_symbol=' '
+    local prompt_char="${purple}>_${reset_color} "
+
+    PS1="\n${user_host}${prompt_symbol}${ruby} ${git_branch} ${return_status}\n${prompt_char}"
+}
+
+PROMPT_COMMAND=prompt
