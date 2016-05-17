05385d43d702b05faa381b1977e03b02429f9e19
Matt Broyles <matt.broyles2@gmail.com>
2016-05-17 16:10:54 -0500

    Moved killrails to plugins, added newline, added meta information, cut indentation to 2 spaces.

---
+++ plugins/available/killrails.plugin.bash
@@ -0,0 +1,15 @@
+cite about-plugin 
+about-plugin 'Function to kill a daemonized Rails server.'
+
+# Quick function to kill a daemonized Rails server
+function killrails() {
+  railsPid="$(cat tmp/pids/server.pid)"
+  if [ ! -z "$railsPid" ]; then
+    echo "[OK] Rails Server Process Id : ${railsPid}"
+	kill -9 $railsPid
+	echo "[OK] Process Killed"
+  else
+	echo "[FAIL] Error killing Rails server"
+	return 1
+  fi
+}
