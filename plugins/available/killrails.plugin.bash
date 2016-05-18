76d026f2adb89a1edf3ba9896f009c379df7a78a
Matt Broyles <mebroyles@crimson.ua.edu>
2016-05-18 09:33:12 -0500

    added about and group metadata

--- plugins/available/killrails.plugin.bash
+++ plugins/available/killrails.plugin.bash
@@ -3,6 +3,9 @@ about-plugin 'Function to kill a daemonized Rails server.'
 
 # Quick function to kill a daemonized Rails server
 function killrails() {
+  about 'Searches for a daemonized Rails server in tmp/pids and attempts to kill it.'
+  group 'server'
+
   railsPid="$(cat tmp/pids/server.pid)"
   if [ ! -z "$railsPid" ]; then
     echo "[OK] Rails Server Process Id : ${railsPid}"
