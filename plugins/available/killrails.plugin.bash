497f1a952016a38f8e61c5a8b175fa8fc2bef148
Nils Winkler <nils@nilswinkler.com>
2016-05-19 09:03:01 +0200

    Fixed indentation
    It was using a mixture of tabs and spaces. Converted to 2 spaces.

--- plugins/available/killrails.plugin.bash
+++ plugins/available/killrails.plugin.bash
@@ -1,4 +1,4 @@
-cite about-plugin 
+cite about-plugin
 about-plugin 'Function to kill a daemonized Rails server.'
 
 # Quick function to kill a daemonized Rails server
@@ -9,10 +9,10 @@ function killrails() {
   railsPid="$(cat tmp/pids/server.pid)"
   if [ ! -z "$railsPid" ]; then
     echo "[OK] Rails Server Process Id : ${railsPid}"
-	kill -9 $railsPid
-	echo "[OK] Process Killed"
+    kill -9 $railsPid
+    echo "[OK] Process Killed"
   else
-	echo "[FAIL] Error killing Rails server"
-	return 1
+    echo "[FAIL] Error killing Rails server"
+    return 1
   fi
 }
