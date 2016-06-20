fe8254b574f522a5aa96cbdc451ff9382bc6d630
Matt Broyles <mebroyles@crimson.ua.edu>
2016-06-20 10:32:01 -0500

    Moved gittowork to git.plugin.bash, removed gittowork.plugin.bash

--- plugins/available/git.plugin.bash
+++ plugins/available/git.plugin.bash
@@ -198,3 +198,25 @@ else
 fi
 }
 
+function gittowork() {
+  about 'Places the latest .gitignore file for a given project type in the current directory, or concatenates onto an existing .gitignore'
+  group 'git'
+
+  result=$(curl -L "Https://www.gitignore.io/api/$1" 2>/dev/null)
+
+  if [[ $result =~ ERROR ]]; then
+    echo "Query '$1' has no match. See a list of possible queries with 'gittowork List'"
+  elif [[ $1 = list ]]; then
+    echo "$result"
+  else
+    if [[ -f .gitignore ]]; then
+      result=`echo "$result" | grep -v "# Created by http://www.gitignore.io"`
+      echo ".gitignore already exists, appending..."
+      echo "$result" >> .gitignore
+    else
+      echo "$result" > .gitignore
+    fi
+  fi
+}
+
+
