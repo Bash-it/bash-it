932a9512943ed4d840914411924ebaef5a0d5e87
Nils Winkler <nils@nilswinkler.com>
2016-06-21 13:16:43 +0200

    Added missing meta tags for gittowork
    Also fixed some typos in the function.

--- plugins/available/git.plugin.bash
+++ plugins/available/git.plugin.bash
@@ -201,11 +201,13 @@ fi
 function gittowork() {
   about 'Places the latest .gitignore file for a given project type in the current directory, or concatenates onto an existing .gitignore'
   group 'git'
+  param '1: the language/type of the project, used for determining the contents of the .gitignore file'
+  example '$ gittowork java'
 
-  result=$(curl -L "Https://www.gitignore.io/api/$1" 2>/dev/null)
+  result=$(curl -L "https://www.gitignore.io/api/$1" 2>/dev/null)
 
   if [[ $result =~ ERROR ]]; then
-    echo "Query '$1' has no match. See a list of possible queries with 'gittowork List'"
+    echo "Query '$1' has no match. See a list of possible queries with 'gittowork list'"
   elif [[ $1 = list ]]; then
     echo "$result"
   else
