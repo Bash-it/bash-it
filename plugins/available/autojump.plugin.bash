577b89ca2c797387c26e19e0108aa844218d8efd
sveneh <sven@styla.com>
2016-07-02 19:19:55 +0200

    added Debian / Ubuntu support for autojump

--- plugins/available/autojump.plugin.bash
+++ plugins/available/autojump.plugin.bash
@@ -1,8 +1,13 @@
 cite about-plugin
 about-plugin 'Autojump configuration, see https://github.com/wting/autojump for more details'
 
-# Only supports the Homebrew variant at the moment.
+# Only covers MacOS brew and Debian installs
 # Feel free to provide a PR to support other install locations
 if command -v brew &>/dev/null && [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]]; then
-  . $(brew --prefix)/etc/profile.d/autojump.sh
+  source $(brew --prefix)/etc/profile.d/autojump.sh
+fi
+
+# Debian / Ubuntu - install it via apt-get install autojump
+if [[ -s /usr/share/doc/autojump/README.Debian ]]; then
+  source /usr/share/autojump/autojump.sh
 fi
