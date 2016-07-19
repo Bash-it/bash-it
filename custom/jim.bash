6c9af9c65c286563427006670ce303bcc8825689
Jim Whimpey <jwhimpey@slack-corp.com>
2016-07-18 18:49:49 -0700

    Added Strava related stuff

---
+++ custom/jim.bash
@@ -0,0 +1,5 @@
+# Ennvironment variables just just for me
+export STRAVA_WRITE_AUTH_TOKEN="4b812f0e2c83bc9a823cd034507e79dada09d93a"
+
+# Aliases for shit
+alias commute="node ~/Sites/commute-button/index.js"
