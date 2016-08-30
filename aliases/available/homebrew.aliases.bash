92f6af52bb322d75f865e685260d9ec28886655f
Jack Letourneau <github@jlet.org>
2016-08-30 10:27:05 -0400

    Use --cleanup on upgrade in Homebrew “bupc” alias.

--- aliases/available/homebrew.aliases.bash
+++ aliases/available/homebrew.aliases.bash
@@ -4,7 +4,7 @@ cite 'about-alias'
 about-alias 'homebrew abbreviations'
 
 alias bup='brew update && brew upgrade --all'
-alias bupc='brew update && brew upgrade --all && brew cleanup'
+alias bupc='brew update && brew upgrade --all --cleanup'
 alias bout='brew outdated'
 alias bin='brew install'
 alias brm='brew uninstall'
