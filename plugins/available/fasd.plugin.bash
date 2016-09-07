5c69d798100df9914167a8f88c940f50085e8dab
Tomas Slusny <slusnucky@gmail.com>
2016-09-07 10:17:40 +0200

    Recognize fasd when sourced
    According to fasd README, fasd can be also used by sourcing it and not only by including it into path. This patch will ensure that fasd will be properly recognized, even when it is sourced and not included in path.

--- plugins/available/fasd.plugin.bash
+++ plugins/available/fasd.plugin.bash
@@ -2,7 +2,7 @@ cite about-plugin
 about-plugin 'initialize fasd (see https://github.com/clvv/fasd)'
 
 __init_fasd() {
-  which fasd &> /dev/null
+  command -v fasd &> /dev/null
   if [ $? -eq 1 ]; then
     echo -e "You must install fasd before you can use this plugin"
     echo -e "See: https://github.com/clvv/fasd"
