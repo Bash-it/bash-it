b32cb8fee2dd26ddbf900b34c6b6941fce2b4fc2
MunifTanjim <MunifTanjim@users.noreply.github.com>
2016-10-10 13:45:34 +0600

    fix rbenv plugin according to PR comments

--- plugins/available/rbenv.plugin.bash
+++ plugins/available/rbenv.plugin.bash
@@ -3,7 +3,7 @@
 cite about-plugin
 about-plugin 'load rbenv, if you are using it'
 
-pathmunge $HOME/.rbenv/bin
+pathmunge "$HOME"/.rbenv/bin
 [ -x `which rbenv` ] && eval "$(rbenv init -)"
 
-[ -d $HOME/.rbenv/plugins/ruby-build ] && pathmunge $HOME/.rbenv/plugins/ruby-build/bin
+[ -d "$HOME"/.rbenv/plugins/ruby-build ] && pathmunge "$HOME"/.rbenv/plugins/ruby-build/bin
