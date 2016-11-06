5a48ef8e681eb59e847784ea301d849d1a54732c
Ilan Erenstein <ilan.erenstein@move.com>
2016-11-06 08:21:22 -0800

    Add vault common aliases

---
+++ aliases/available/vault.aliases.bash
@@ -0,0 +1,15 @@
+cite 'about-alias'
+about-alias 'vault aliases'
+
+# Aliases
+alias vd="vault delete"
+alias vl="vault list"
+alias vr="vault read"
+alias vrn="vault renew"
+alias vrv="vault revoke"
+alias vsrv="vault server"
+alias vs="vault status"
+alias vv="vault version"
+alias vw="vault write"
+alias vag="vault auth -method=github"
+alias vrv="vault read -field=value"
