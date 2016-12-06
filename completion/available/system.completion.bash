8161e4e233fa01de27f522f063c3e3f82b9d0419
Patrice Ferlet <metal3d@gmail.com>
2016-12-07 00:10:18 +0100

    Add other bash completion importation for other distribution
    Fedora import that script from /etc/bashrc script that can import /etc/profile.d/*.sh
    
    See issue #849

--- completion/available/system.completion.bash
+++ completion/available/system.completion.bash
@@ -7,6 +7,12 @@ if [ -f /etc/bash_completion ]; then
   . /etc/bash_completion
 fi
 
+# Some distribution makes use of a profile.d script to import completion.
+if [ -f /etc/profile.d/bash_completion.sh ]; then
+  . /etc/profile.d/bash_completion.sh
+fi
+
+
 if [ $(uname) = "Darwin" ] && command -v brew &>/dev/null ; then
   BREW_PREFIX=$(brew --prefix)
 
