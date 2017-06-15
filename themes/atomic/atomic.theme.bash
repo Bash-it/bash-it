d4bc67ffdd9542acad7e11c663fb4c5d6bd87a12
Luis Felipe Sánchez <lfelipe1501@gmail.com>
2017-06-15 15:37:01 -0500

    Update Theme
    Minor Fixes

--- themes/atomic/atomic.theme.bash
+++ themes/atomic/atomic.theme.bash
@@ -178,7 +178,7 @@ ___atomic_prompt_battery() {
   ac_adapter_disconnected && info="-"
   ac_adapter_connected && info="+"
   info+=$batp
-  [ "$info" == "+100" ] && info="AC"
+  [ "$batp" -eq 100 ] || [ "$batp" -gt 100 ] && info="AC"
   printf "%s|%s|%s|%s" "${color}" "${info}" "${bold_white}" "${box}"
 }
 
