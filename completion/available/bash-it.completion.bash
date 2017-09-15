752d4afe4149dafee3fa52401a29b9bfdcf3c571
Nils Winkler <nils@nilswinkler.com>
2017-09-15 08:10:15 +0200

    Tabs to spaces

--- completion/available/bash-it.completion.bash
+++ completion/available/bash-it.completion.bash
@@ -2,112 +2,112 @@
 
 _bash-it-comp-enable-disable()
 {
-	local enable_disable_args="alias plugin completion"
-	COMPREPLY=( $(compgen -W "${enable_disable_args}" -- ${cur}) )
+  local enable_disable_args="alias plugin completion"
+  COMPREPLY=( $(compgen -W "${enable_disable_args}" -- ${cur}) )
 }
 
 _bash-it-comp-list-available-not-enabled()
 {
-	subdirectory="$1"
+  subdirectory="$1"
 
-	local available_things=$(for f in `compgen -G "${BASH_IT}/$subdirectory/available/*.bash" | sort`;
-		do
-			if [ ! -e "${BASH_IT}/$subdirectory/enabled/"$(basename $f) ] && [ ! -e "${BASH_IT}/$subdirectory/enabled/"*$BASH_IT_LOAD_PRIORITY_SEPARATOR$(basename $f) ]
-			then
-				basename $f | cut -d'.' -f1
-			fi
-		done)
+  local available_things=$(for f in `compgen -G "${BASH_IT}/$subdirectory/available/*.bash" | sort`;
+    do
+      if [ ! -e "${BASH_IT}/$subdirectory/enabled/"$(basename $f) ] && [ ! -e "${BASH_IT}/$subdirectory/enabled/"*$BASH_IT_LOAD_PRIORITY_SEPARATOR$(basename $f) ]
+      then
+        basename $f | cut -d'.' -f1
+      fi
+    done)
 
-	COMPREPLY=( $(compgen -W "all ${available_things}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "all ${available_things}" -- ${cur}) )
 }
 
 _bash-it-comp-list-enabled()
 {
-	subdirectory="$1"
+  subdirectory="$1"
 
-	local enabled_things=$(for f in `compgen -G "${BASH_IT}/$subdirectory/enabled/*.bash" | sort`;
-		do
-			basename $f | cut -d'.' -f1 | sed -e "s/^[0-9]*---//g"
-		done)
+  local enabled_things=$(for f in `compgen -G "${BASH_IT}/$subdirectory/enabled/*.bash" | sort`;
+    do
+      basename $f | cut -d'.' -f1 | sed -e "s/^[0-9]*---//g"
+    done)
 
-	COMPREPLY=( $(compgen -W "all ${enabled_things}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "all ${enabled_things}" -- ${cur}) )
 }
 
 _bash-it-comp-list-available()
 {
-	subdirectory="$1"
+  subdirectory="$1"
 
-	local enabled_things=$(for f in `compgen -G "${BASH_IT}/$subdirectory/available/*.bash" | sort`;
-		do
-			basename $f | cut -d'.' -f1
-		done)
+  local enabled_things=$(for f in `compgen -G "${BASH_IT}/$subdirectory/available/*.bash" | sort`;
+    do
+      basename $f | cut -d'.' -f1
+    done)
 
-	COMPREPLY=( $(compgen -W "${enabled_things}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "${enabled_things}" -- ${cur}) )
 }
 
 _bash-it-comp()
 {
-	local cur prev opts prevprev
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	chose_opt="${COMP_WORDS[1]}"
-	file_type="${COMP_WORDS[2]}"
-	opts="help show enable disable update search migrate"
-	case "${chose_opt}" in
-		show)
-			local show_args="plugins aliases completions"
-			COMPREPLY=( $(compgen -W "${show_args}" -- ${cur}) )
-			return 0
-			;;
-		help)
-			local help_args="plugins aliases completions migrate update"
-			COMPREPLY=( $(compgen -W "${help_args}" -- ${cur}) )
-			return 0
-			;;
+  local cur prev opts prevprev
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  chose_opt="${COMP_WORDS[1]}"
+  file_type="${COMP_WORDS[2]}"
+  opts="help show enable disable update search migrate"
+  case "${chose_opt}" in
+    show)
+      local show_args="plugins aliases completions"
+      COMPREPLY=( $(compgen -W "${show_args}" -- ${cur}) )
+      return 0
+      ;;
+    help)
+      local help_args="plugins aliases completions migrate update"
+      COMPREPLY=( $(compgen -W "${help_args}" -- ${cur}) )
+      return 0
+      ;;
     update | search | migrate)
       return 0
       ;;
-		enable | disable)
-			if [ x"${chose_opt}" == x"enable" ];then
-				suffix="available-not-enabled"
-			else
-				suffix="enabled"
-			fi
-			case "${file_type}" in
-				alias)
-						_bash-it-comp-list-${suffix} aliases
-						return 0
-						;;
-				plugin)
-						_bash-it-comp-list-${suffix} plugins
-						return 0
-						;;
-				completion)
-						_bash-it-comp-list-${suffix} completion
-						return 0
-						;;
-				*)
-						_bash-it-comp-enable-disable
-						return 0
-						;;
-			esac
-			;;
-		aliases)
-			prevprev="${COMP_WORDS[COMP_CWORD-2]}"
+    enable | disable)
+      if [ x"${chose_opt}" == x"enable" ];then
+        suffix="available-not-enabled"
+      else
+        suffix="enabled"
+      fi
+      case "${file_type}" in
+        alias)
+            _bash-it-comp-list-${suffix} aliases
+            return 0
+            ;;
+        plugin)
+            _bash-it-comp-list-${suffix} plugins
+            return 0
+            ;;
+        completion)
+            _bash-it-comp-list-${suffix} completion
+            return 0
+            ;;
+        *)
+            _bash-it-comp-enable-disable
+            return 0
+            ;;
+      esac
+      ;;
+    aliases)
+      prevprev="${COMP_WORDS[COMP_CWORD-2]}"
 
-			case "${prevprev}" in
-				help)
-					_bash-it-comp-list-available aliases
-					return 0
-					;;
-			esac
-			;;
-	esac
+      case "${prevprev}" in
+        help)
+          _bash-it-comp-list-available aliases
+          return 0
+          ;;
+      esac
+      ;;
+  esac
 
-	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
 
-	return 0
+  return 0
 }
 
 # Activate completion for bash-it and its common misspellings
