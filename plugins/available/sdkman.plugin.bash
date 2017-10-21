083974484e9d13d4dbbb7aa6f81ac0358a8a612b
Michael Masi <mmasi-attivio@users.noreply.github.com>
2017-10-21 19:15:55 -0400

    Use custom SDKMAN_DIR if set

--- plugins/available/sdkman.plugin.bash
+++ plugins/available/sdkman.plugin.bash
@@ -1,5 +1,9 @@
 cite about-plugin
 about-plugin 'Load Software Development Kit Manager'
 
-export SDKMAN_DIR="$HOME/.sdkman"
+# Use $SDKMAN_DIR if defined,
+# otherwise default to ~/.sdkman
+export SDKMAN_DIR=${SDKMAN_DIR:-$HOME/.sdkman}
+
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+
