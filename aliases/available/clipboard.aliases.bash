9e9978033ce91d565d9321079fd58b6f745e2f0c
Eudaimonstro <SGCouch1@gmail.com>
2018-03-02 19:05:00 -0800

    Breaking up alias declarations into their own lines so that bash-it help aliases will register them

--- aliases/available/clipboard.aliases.bash
+++ aliases/available/clipboard.aliases.bash
@@ -4,7 +4,9 @@ about-alias 'pbcopy and pbpaste shortcuts to linux'
 case $OSTYPE in
   linux*)
     XCLIP=$(command -v xclip)
-    [[ $XCLIP ]] && alias pbcopy="$XCLIP -selection clipboard" && alias pbpaste="$XCLIP -selection clipboard -o"
+    [[ $XCLIP ]] && \
+    alias pbcopy="$XCLIP -selection clipboard" && \
+    alias pbpaste="$XCLIP -selection clipboard -o"
     ;;   
 esac
 
