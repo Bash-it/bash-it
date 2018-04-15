e6a6bb5debdd3485c9d150ae44ef3fdda0cc511e
Jason Al-Mansor <jalmansor@gmail.com>
2018-04-15 14:32:18 -0400

    fix hostname in remaining powerline themes

--- themes/powerline-naked/powerline-naked.theme.bash
+++ themes/powerline-naked/powerline-naked.theme.bash
@@ -47,6 +47,8 @@ THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:="%H:%M:%S"}
 IN_VIM_THEME_PROMPT_COLOR=245
 IN_VIM_THEME_PROMPT_TEXT="vim"
 
+HOST_THEME_PROMPT_COLOR=254
+
 POWERLINE_PROMPT=${POWERLINE_PROMPT:="user_info scm python_venv ruby cwd"}
 
 safe_append_prompt_command __powerline_prompt_command
