0028dc7661870954e6042305ce7a5bff9d71779c
Nils Winkler <nils@nilswinkler.com>
2018-05-31 17:57:09 +0200

    Fixed indentation to two spaces, like in the rest of the file

--- bash_it.sh
+++ bash_it.sh
@@ -4,18 +4,18 @@
 # Only set $BASH_IT if it's not already set
 if [ -z "$BASH_IT" ];
 then
-    # Setting $BASH to maintain backwards compatibility
-    # TODO: warn users that they should upgrade their .bash_profile
-    export BASH_IT=$BASH
-    export BASH="$(bash -c 'echo $BASH')"
+  # Setting $BASH to maintain backwards compatibility
+  # TODO: warn users that they should upgrade their .bash_profile
+  export BASH_IT=$BASH
+  export BASH="$(bash -c 'echo $BASH')"
 fi
 
 # For backwards compatibility, look in old BASH_THEME location
 if [ -z "$BASH_IT_THEME" ];
 then
-    # TODO: warn users that they should upgrade their .bash_profile
-    export BASH_IT_THEME="$BASH_THEME";
-    unset $BASH_THEME;
+  # TODO: warn users that they should upgrade their .bash_profile
+  export BASH_IT_THEME="$BASH_THEME";
+  unset $BASH_THEME;
 fi
 
 # Load composure first, so we support function metadata
@@ -81,7 +81,7 @@ done
 
 unset config_file
 if [[ $PROMPT ]]; then
-    export PS1="\[""$PROMPT""\]"
+  export PS1="\[""$PROMPT""\]"
 fi
 
 # Adding Support for other OSes
