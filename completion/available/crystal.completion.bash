76de592cafb021c3168d89b2dd5d4571eb0e895c
Travis Swicegood <travis@domain51.com>
2018-08-30 18:43:00 -0500

    Add Crystal auto-completion on Mac

---
+++ completion/available/crystal.completion.bash
@@ -0,0 +1,8 @@
+if which crystal >/dev/null 2>&1; then
+  BREW_PREFIX=$(brew --prefix)
+
+  if [ -f "$BREW_PREFIX"/etc/bash_completion.d/crystal ]; then
+    . "$BREW_PREFIX"/etc/bash_completion.d/crystal
+  fi
+
+fi
