e6d6b47abea489acc3fb658ff0d5f651cbe76039
penguin2048 <rishabhkr1729@gmail.com>
2018-09-10 19:20:22 +0530

    Add virtual env support for elixr theme

--- themes/elixr/elixr.theme.bash
+++ themes/elixr/elixr.theme.bash
@@ -17,7 +17,7 @@ RVM_THEME_PROMPT_SUFFIX=" d|"
 BOLD="\[\e[1m\]"
 
 function prompt_command() {
-  PS1="\n${bold_cyan}$(scm_prompt_char_info) ${bold_cyan}\w :${reset_color}${normal}${BOLD} "
+  PS1="\n${bold_cyan}$(scm_prompt_char_info)$(virtualenv_prompt) ${bold_cyan}\w :${reset_color}${normal}${BOLD} "
 }
 
 safe_append_prompt_command prompt_command
