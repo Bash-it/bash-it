025554d79d9e67e6846095107a96f16cbe912ecf
Saulius Gurklys <s4uliu5@gmail.com>
2018-10-06 16:44:37 +0300

    Add minimal HG prompt to powerline themes

--- themes/powerline/powerline.base.bash
+++ themes/powerline/powerline.base.bash
@@ -86,6 +86,8 @@ function __powerline_scm_prompt {
       scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
     elif [[ "${SCM_P4_CHAR}" == "${SCM_CHAR}" ]]; then
       scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
+    elif [[ "${SCM_HG_CHAR}" == "${SCM_CHAR}" ]]; then
+      scm_prompt+="${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
     fi
     echo "${scm_prompt}${scm}|${color}"
   fi
