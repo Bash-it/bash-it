992b87b816c57e6ac2ea195d18cb1ba6888b5552
Travis Swicegood <travis@domain51.com>
2018-12-05 21:00:59 -0600

    Shellcheck clean up based on comments from @nwinkler

--- scripts/reloader.bash
+++ scripts/reloader.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
-pushd "${BASH_IT}" >/dev/null
+pushd "${BASH_IT}" >/dev/null || exit 1
 
 # TODO: Add debugging output
 
 if [ "$1" != "false" ] && [ -d "./enabled" ]; then
-  for _bash_it_config_file in $(ls ./enabled/*${1}.bash 2>/dev/null); do
+  for _bash_it_config_file in $(sort <(compgen -G "./enabled/*${1}.bash")); do
     if [ -e "${_bash_it_config_file}" ]; then
+      # shellcheck source=/dev/null
       source $_bash_it_config_file
     else
       echo "Unable to read ${_bash_it_config_file}" > /dev/stderr
@@ -16,8 +17,9 @@ fi
 
 if [ ! -z "${2}" ] && [ -d "${2}/enabled" ]; then
   # TODO: We should warn users they're using legacy enabling
-  for _bash_it_config_file in $(ls ./${2}/enabled/*.bash 2>/dev/null); do
+  for _bash_it_config_file in $(sort <(compgen -G "./${2}/enabled/*.bash")); do
     if [ -e "$_bash_it_config_file" ]; then
+      # shellcheck source=/dev/null
       source "$_bash_it_config_file"
     else
       # TODO Display an error?
@@ -27,4 +29,4 @@ if [ ! -z "${2}" ] && [ -d "${2}/enabled" ]; then
 fi
 
 unset _bash_it_config_file
-popd >/dev/null
+popd >/dev/null || exit 1
