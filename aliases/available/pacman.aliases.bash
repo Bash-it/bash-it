fa7d91bd8b5f65ed4ad980d81391976480a34831
NiilShon <51centimos@gmail.com>
2019-01-22 23:35:05 +0100

    Create pacman.aliases.bash
    On Spanish comment

---
+++ aliases/available/pacman.aliases.bash
@@ -0,0 +1,34 @@
+#NIIL78
+cite 'about-alias'
+about-alias 'Pacman system Arch'
+
+############################################################
+## PACMAN ALIAS                                            #
+############################################################
+alias Pacman1='sudo pacman -Syu'#Actualizacion
+alias Pacman2='sudo pacman -Ss' #Busca paquetes.
+alias Pacman3='sudo pacman -S'  #Instalar 
+alias pacman4='sudo pacman -Rs'  #Desinstala
+alias Pacman5='sudo pacman -Syy'#Obliga a actualizar los repos
+
+############################################################
+# YAY                                                      #
+############################################################
+alias Yay1='sudo yay -Syu'#Actualizacion
+alias Yay2='sudo yay -Ss' #Busca paquetes.
+alias Yay3='sudo yay -S'  #Instalar 
+alias Yay='sudo yay -Rs'  #Desinstala
+alias Yay5='sudo yay -Syy'#Obliga a actualizar los repos
+
+############################################################
+# PAKKU                                                    #
+############################################################
+alias Yay1='sudo pakku -Syu'#Actualizacion
+alias Yay2='sudo pakku -Ss' #Busca paquetes.
+alias Yay3='sudo pakku -S'  #Instalar 
+alias Yay='sudo pukku -Rs'  #Desinstala
+alias Yay5='sudo pakku -Syy'#Obliga a actualizar los repos
+
+##################################
+#D I S F R U T A-T U-S Y S T E M #
+##################################
