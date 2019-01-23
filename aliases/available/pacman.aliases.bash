819f16f4a72e74018c9642a22e9912a730737154
NiilShon <51centimos@gmail.com>
2019-01-23 14:44:12 +0100

    Update pacman.aliases.bash

--- aliases/available/pacman.aliases.bash
+++ aliases/available/pacman.aliases.bash
@@ -7,29 +7,29 @@ about-alias 'Pacman system Arch'
 ## PACMAN ALIAS     USE:Pacman0                            #
 ############################################################
 alias Pacman0='echo -e "Pacman1 Actualizar/update";echo -e "Pacman2 Buscar/Search";echo -e "Pacman3 Install";echo -e "Pacman4 Desinstalar/Unistall";echo -e "Pacman5 Fuerza Actualizar/Forze update";'
-alias Pacman1='sudo pacman -Syu' #Actualizacion
-alias Pacman2='sudo pacman -Ss'  #Busca paquetes.
-alias Pacman3='sudo pacman -S'   #Instalar
-alias pacman4='sudo pacman -Rs'  #Desinstala
-alias Pacman5='sudo pacman -Syy' #Obliga a actualizar los repos
+alias Pacman1='sudo pacman -Syu' #Actualizacion /Update
+alias Pacman2='sudo pacman -Ss'  #Busca paquetes. /Search Packages
+alias Pacman3='sudo pacman -S'   #Instalar /Install
+alias pacman4='sudo pacman -Rs'  #Desinstala /Unistall
+alias Pacman5='sudo pacman -Syy' #Obliga a actualizar los repos /Force Update
 ############################################################
 # YAY                          USE:Yay0                    #
 ############################################################
 alias Yay0='echo -e "Yay1 Actualizar/update";echo -e "Yay2 Buscar/Search";echo -e "Yay3 Install";echo -e "Yay4 Desinstalar/Unistall";echo -e "Yay5 Fuerza Actualizar/Forze update";'
-alias Yay1='sudo yay -Syu' #Actualizacion
-alias Yay2='sudo yay -Ss'  #Busca paquetes
-alias Yay3='sudo yay -S'   #Instalar 
-alias Yay4='sudo yay -Rs'   #Desinstala
-alias Yay5='sudo yay -Syy' #Obliga a actualizar los repos
+alias Yay1='sudo yay -Syu' #Actualizacion /Update
+alias Yay2='sudo yay -Ss'  #Busca paquetes /Search package
+alias Yay3='sudo yay -S'   #Instalar  /install 
+alias Yay4='sudo yay -Rs'   #Desinstala /unistall 
+alias Yay5='sudo yay -Syy' #Obliga a actualizar los repos /force update
 ############################################################
 # PAKKU                      USE:Pakku0                    #
 ############################################################
 alias Pakku0='echo -e "Pakku1 Actualizar/update";echo -e "Pakku2 Buscar/Search";echo -e "Pakku3 Install";echo -e "Pakku4 Desinstalar/Unistall";echo -e "Pakku5 Fuerza Actualizar/Forze update";'
-alias Pakku1='sudo pakku -Syu' #Actualizacion
-alias Pakku2='sudo pakku -Ss'  #Busca paquetes.
-alias Pakku3='sudo pakku -S'   #Instalar 
-alias Pakku4='sudo pukku -Rs'   #Desinstala
-alias Pakku5='sudo pakku -Syy' #Obliga a actualizar los repos
+alias Pakku1='sudo pakku -Syu' #Actualizacion /update
+alias Pakku2='sudo pakku -Ss'  #Busca paquetes. /search packages
+alias Pakku3='sudo pakku -S'   #Instalar  /install 
+alias Pakku4='sudo pukku -Rs'   #Desinstala /unistall
+alias Pakku5='sudo pakku -Syy' #Obliga a actualizar los repos /force update
 
 ##################################
 #D I S F R U T A-T U-S Y S T E M #
