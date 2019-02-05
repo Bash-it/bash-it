f362d52935abf0957fbc928182b24ad998317344
NiilShon <51centimos@gmail.com>
2019-02-05 22:19:50 +0100

    Update nmap.aliases.bash
    You need to learn Spanish.

--- nmap.aliases.bash
+++ nmap.aliases.bash
@@ -2,17 +2,16 @@ cite 'about-alias'
 about-alias 'Nmap scanner'
 
 ###############################
-# Usa Nmap0 para leer el menu #
 # Use Nmap0 for read info     #
 ###############################
 
-alias Nmap0='echo -e "Nmap1 Geografico/Geographical Scan";echo -e "Nmap2 Versines/Versions";echo -e "Nmap3 Paranoico/Paranoic";echo -e "Nmap4 Furtivo/Sneaky";echo -e "Nmap5 Cortes/Polite";echo -e "Nmap6 Normal";echo -e "Nmap 7 Agresivo/Agresive";echo -e "Nmap8 Insano/Insane";echo -e "Nmap9 WEB (vulscan)";'
-alias Nmap1='echo "geografico/GeoGrafic"; sudo nmap --script=asn-query,whois,ip-geolocation-maxmind'
+alias Nmap0='echo -e "Nmap1 Geographical Scan";echo -e "Nmap2 Versions";echo -e "Nmap3 Paranoic";echo -e "Nmap4 Sneaky";echo -e "Nmap5 Polite";echo -e "Nmap6 Normal";echo -e "Nmap 7 Agresive";echo -e "Nmap8 Insane";echo -e "Nmap9 WEB (vulscan)";'
+alias Nmap1='echo "GeoGrafic"; sudo nmap --script=asn-query,whois,ip-geolocation-maxmind'
 alias Nmap2='echo "script"; sudo nmap -sV -sC' 
-alias Nmap3='echo "Paranoico/Paranoid (Lento/slow) "; sudo nmap -sSV -sC -p- -T0 '
-alias Nmap4='echo "Furtivo/Sneaky (Lento/slow)"; sudo nmap -sSV -sC -p- -T1'
-alias Nmap5='echo "Cortes/Polite (lento/slow)"; sudo nmap -sSV -sC -p- -T2'
-alias Nmap6='echo "Normal (Normal)"; sudo nmap -sSV -sC -p- -T3'
-alias Nmap7='echo "Agresivo/Agresive (Rapidito/Quick)"; sudo nmap -sSV -sC -p- -T4'
-alias Nmap8='echo "Insano/insane (Rapidito/Quick)"; sudo nmap -sSV -sC -p- -T5'
+alias Nmap3='echo "Paranoid slow"; sudo nmap -sSV -sC -p- -T0 '
+alias Nmap4='echo "Sneaky slow"; sudo nmap -sSV -sC -p- -T1'
+alias Nmap5='echo "Polite slow"; sudo nmap -sSV -sC -p- -T2'
+alias Nmap6='echo "Normal"; sudo nmap -sSV -sC -p- -T3'
+alias Nmap7='echo "Agresive Quick"; sudo nmap -sSV -sC -p- -T4'
+alias Nmap8='echo "insane Quick"; sudo nmap -sSV -sC -p- -T5'
 alias Nmap9='echo "WEB(vulscan)": sudo nmap -sV --script=vulscan/vulscan.nse'
