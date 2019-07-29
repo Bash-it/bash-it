5dbcbce95398b5b3f05a59b214b1e0eec265cc35
Jacob Hrbek <werifgx@gmail.com>
2019-07-30 01:56:05 +0200

    Fixes syntax

--- aliases/available/apt.aliases.bash
+++ aliases/available/apt.aliases.bash
@@ -13,18 +13,18 @@ function _set_pkg_aliases()
 
 	# Aliases
 	if [ -x $(which apt) ]; then
-		alias apti='${BASHIT_USE_ROOT:+sudo }apt-get install' # Advanced Packating Tool Install
-		alias aptr='${BASHIT_USE_ROOT:+sudo }apt-get remove' # Advanced Packating Tool Remove
-		alias apts='${BASHIT_USE_ROOT:+sudo }apt-cache search' # Advanced Packating Tool Search
-		alias aptfu='${BASHIT_USE_ROOT:+sudo }apt-get update -y && ${BASHIT_USE_ROOT:+sudo }apt-get upgrade -y && ${BASHIT_USE_ROOT:+sudo }apt-get dist-upgrade -y && ${BASHIT_USE_ROOT:+sudo }apt-get autoremove -y' # Advanced Packaging Tool Full Update/Upgrade
-		alias aptar='${BASHIT_USE_ROOT:+sudo }apt-get autoremove -y' # Advanced Packaging Tool Auto Remove
-		alias aptiv='${BASHIT_USE_ROOT:+sudo }apt-get install -V' # Advanced Packaging Tool Install Verbose
-		alias aptupd='${BASHIT_USE_ROOT:+sudo }apt-get update -y' # Advanced Packaging Tool UPDate
-		
-		alias aptupg='${BASHIT_USE_ROOT:+sudo }apt-get dist-upgrade -V'
+		alias apti="${BASHIT_USE_ROOT:+sudo }apt-get install" # Advanced Packating Tool Install
+		alias aptr="${BASHIT_USE_ROOT:+sudo }apt-get remove" # Advanced Packating Tool Remove
+		alias apts="${BASHIT_USE_ROOT:+sudo }apt-cache search" # Advanced Packating Tool Search
+		alias aptfu="${BASHIT_USE_ROOT:+sudo }apt-get update -y && ${BASHIT_USE_ROOT:+sudo }apt-get upgrade -y && ${BASHIT_USE_ROOT:+sudo }apt-get dist-upgrade -y && ${BASHIT_USE_ROOT:+sudo }apt-get autoremove -y" # Advanced Packaging Tool Full Update/Upgrade
+		alias aptar="${BASHIT_USE_ROOT:+sudo }apt-get autoremove -y" # Advanced Packaging Tool Auto Remove
+		alias aptiv="${BASHIT_USE_ROOT:+sudo }apt-get install -V" # Advanced Packaging Tool Install Verbose
+		alias aptupd="${BASHIT_USE_ROOT:+sudo }apt-get update -y" # Advanced Packaging Tool UPDate
+
+		alias aptupg="${BASHIT_USE_ROOT:+sudo }apt-get dist-upgrade -V"
 		alias aptshow='apt-cache show'
-		alias aptupgd='${BASHIT_USE_ROOT:+sudo }apt-get update && sudo apt-get dist-upgrade -V && sudo apt-get autoremove'
-		alias aptp='${BASHIT_USE_ROOT:+sudo }apt-get remove --purge'#  Advanced Packating Tool Purge
+		alias aptupgd="${BASHIT_USE_ROOT:+sudo }apt-get update && sudo apt-get dist-upgrade -V && sudo apt-get autoremove"
+		alias aptp="${BASHIT_USE_ROOT:+sudo }apt-get remove --purge"#  Advanced Packating Tool Purge
 
 		alias chkup='/usr/lib/update-notifier/apt-check -p --human-readable'
 		alias chkboot='cat /var/run/reboot-required'
