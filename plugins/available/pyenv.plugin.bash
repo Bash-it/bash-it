12c49bd22fc1f1cbe9d48bc969661b0abaffe5d3
Martin Lariz <mlariz@proofpoint.com>
2019-07-29 07:07:58 -0700

    Remove errors if plugin executable not available

--- plugins/available/pyenv.plugin.bash
+++ plugins/available/pyenv.plugin.bash
@@ -4,7 +4,7 @@ about-plugin 'load pyenv, if you are using it'
 export PYENV_ROOT="$HOME/.pyenv"
 pathmunge "$PYENV_ROOT/bin"
 
-[[ `which pyenv` ]] && eval "$(pyenv init - bash)"
+[[ `which pyenv 2>/dev/null` ]] && eval "$(pyenv init - bash)"
 
 #Load pyenv virtualenv if the virtualenv plugin is installed.
 if pyenv virtualenv-init - &> /dev/null; then
