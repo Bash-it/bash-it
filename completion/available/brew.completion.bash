7c73b22e75ac7cceffaec9bc88b6c20d5b5436b6
Hongyi Zhao <hongyi.zhao@gmail.com>
2019-12-19 16:45:39 +0800

    fix completion/available/brew.completion.bash for the git-clone based installation

--- completion/available/brew.completion.bash
+++ completion/available/brew.completion.bash
@@ -8,4 +8,11 @@ if which brew >/dev/null 2>&1; then
   if [ -f "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh ]; then
     . "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh
   fi
+
+  # For the git-clone based installation, see here for more info:
+  # https://github.com/Bash-it/bash-it/issues/1458
+  # https://docs.brew.sh/Shell-Completion   
+  if [ -f "$BREW_PREFIX"/completions/bash/brew ]; then
+    . "$BREW_PREFIX"/completions/bash/brew
+  fi
 fi
