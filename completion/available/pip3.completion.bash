d85a2544299357719859560f3175925049b8ab9e
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-01-09 23:20:28 +0800

    Fix the completion for pip/pip3

--- completion/available/pip3.completion.bash
+++ completion/available/pip3.completion.bash
@@ -1,11 +1,2 @@
-
-# pip bash completion start
-_pip_completion()
-{
-    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   PIP_AUTO_COMPLETE=1 $1 ) )
-}
-complete -o default -F _pip_completion pip3
-# pip bash completion end
-
+# https://pip.pypa.io/en/stable/user_guide/#command-completion
+eval "$(pip3 completion --bash)"
