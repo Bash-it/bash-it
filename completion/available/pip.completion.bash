d54577f992ae8bd3767e6d50dd828bcb934d1a60
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-01-09 23:41:04 +0800

    touch up modified:   completion/available/pip.completion.bash modified:   completion/available/pip3.completion.bash

--- completion/available/pip.completion.bash
+++ completion/available/pip.completion.bash
@@ -1,2 +1,10 @@
 # https://pip.pypa.io/en/stable/user_guide/#command-completion
-eval "$(pip completion --bash)"
+# Of course, you should first install the pip, say on Debian:
+# sudo apt-get install python-pip
+# sudo apt-get install python3-pip
+# If the pip package is installed within virtual environments, say, python managed by pyenv,
+# you should first initilization the corresponding environment.
+# So that the pip/pip3 is in system's path.
+if command -v pip >/dev/null; then
+  eval "$(pip completion --bash)"
+fi
--- completion/available/pip3.completion.bash
+++ completion/available/pip3.completion.bash
@@ -1,2 +1,11 @@
 # https://pip.pypa.io/en/stable/user_guide/#command-completion
-eval "$(pip3 completion --bash)"
+# Of course, you should first install the pip, say on Debian:
+# sudo apt-get install python-pip
+# sudo apt-get install python3-pip
+# If the pip package is installed within virtual environments, say, python managed by pyenv,
+# you should first initilization the corresponding environment.
+# So that the pip/pip3 is in system's path.
+if command -v pip3 >/dev/null; then
+  eval "$(pip3 completion --bash)"
+fi
+
