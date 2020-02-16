72224ab03469bbb7705e39ace9c7f9cb26faeccc
Bingzhang Dai <bingzhangdai@users.noreply.github.com>
2020-02-16 15:39:37 +0800

    Create ~/.ssh and ~/.ssh/config if does not exits

--- plugins/available/ssh.plugin.bash
+++ plugins/available/ssh.plugin.bash
@@ -7,14 +7,15 @@ function add_ssh() {
   param '2: hostname'
   param '3: user'
   group 'ssh'
-
+  [[ $# -ne 3 ]] && echo "add_ssh host hostname user" && return 1
+  [[ ! -d ~/.ssh ]] && mkdir -m 700 ~/.ssh
+  [[ ! -e ~/.ssh/config ]] && touch ~/.ssh/config && chmod 600 ~/.ssh/config
   echo -en "\n\nHost $1\n  HostName $2\n  User $3\n  ServerAliveInterval 30\n  ServerAliveCountMax 120" >> ~/.ssh/config
 }
 
 function sshlist() {
   about 'list hosts defined in ssh config'
   group 'ssh'
-
   awk '$1 ~ /Host$/ {for (i=2; i<=NF; i++) print $i}' ~/.ssh/config
 }
 
