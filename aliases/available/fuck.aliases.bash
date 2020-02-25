57027590738b84dec797cad67ed1a443e6941b08
Matthew Adams <matthew.adams@northscaler.com>
2020-02-25 08:24:14 -0600

    rm thefuck.aliases.bash

--- aliases/available/fuck.aliases.bash
+++ aliases/available/fuck.aliases.bash
@@ -1,4 +1,2 @@
 alias please='sudo $(fc -ln -1)'
-alias plz=please
-alias fuck='sudo'
-alias fucking=fuck
+alias fucking='sudo'
--- aliases/available/thefuck.aliases.bash
+++
@@ -1,5 +0,0 @@
-# https://github.com/nvbn/thefuck
-alias fuck='eval $(thefuck $(fc -ln -1))'
-alias please='fuck'
-alias plz=please
-alias fucking=fuck
