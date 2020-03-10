dc2c9cfd3832050c09b22a7f08ffe99cc8d1af5e
David Farrell <davidpfarrell+github@gmail.com>
2020-03-10 01:32:41 -0700

    Sets GOENV_ROOT early, exports after finding goenv

--- plugins/available/goenv.plugin.bash
+++ plugins/available/goenv.plugin.bash
@@ -1,21 +1,23 @@
 cite about-plugin
 about-plugin 'Init goenv, if installed. Plays nicely with package managers'
 
+# Use a reasonable default to ensure variable is always present
+# NOTE: This (currently) matches goenv's built-in default
+# NOTE: We don't export it yet, since we might still bail
+GOENV_ROOT="${GOENV_ROOT:-${HOME}/.goenv}"
+
 # Look for goenv command
 if ! _command_exists goenv ; then
 	# Add it to the path if we find it, bail otherwise
 	if [ -n "${GOENV_ROOT}" ] && [ -x "${GOENV_ROOT}/bin/goenv" ] ; then
 		pathmunge "${GOENV_ROOT}/bin"
-	elif [ -x "${HOME}/.goenv/bin/goenv" ]; then
-		pathmunge "${HOME}/.goenv/bin"
 	else
 		return
 	fi
 fi
 
-# Use a reasonable default to ensure variable is always present
-# NOTE: This (currently) matches goenv's built-in default
-export GOENV_ROOT="${GOENV_ROOT:-${HOME}/.goenv}"
+# Since we didn't bail, export the value
+export GOENV_ROOT
 
 # Initialize goenv
 # NOTE: In the off-chance that goenv has already been initialized,
