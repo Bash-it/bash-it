7f8652999b4848ef819557b9f40b34d6fb9ccbbc
salex <salex@gmail.com>
2020-03-19 21:27:54 +0800

    fix code review issues.

--- themes/envy/envy.theme.bash
+++ themes/envy/envy.theme.bash
@@ -6,24 +6,14 @@ SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-GIT_THEME_PROMPT_PREFIX=" ${green}| "
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
 GIT_THEME_PROMPT_SUFFIX="${green}|"
 
-# virtualenv prompts
-VIRTUALENV_CHAR="ⓔ "
-VIRTUALENV_THEME_PROMPT_PREFIX=""
+VIRTUALENV_THEME_PROMPT_PREFIX="${green}ⓔ  "
 VIRTUALENV_THEME_PROMPT_SUFFIX=""
 
-# Display virtual environment info
-function virtualenv_prompt {
-  if [[ -n "$VIRTUAL_ENV" ]]; then
-    virtualenv=`basename "$VIRTUAL_ENV"`
-    echo -e "$VIRTUALENV_CHAR$virtualenv "
-  fi
-}
-
 function prompt_command() {
-    PS1="\n${yellow}$(virtualenv_prompt)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+    PS1="\n$(virtualenv_prompt)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
 }
 
 safe_append_prompt_command prompt_command
