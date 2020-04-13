74f053db9d0d9dfa307290dcd3b812e1f2793c75
Wabri <gabriele.puliti@protonmail.com>
2020-04-13 12:51:20 +0200

    Remove (alias): bash_it alias is no more required in general
    The bash_it alias are exported to the relative source, so is no more
    required in the general alias.

--- aliases/available/general.aliases.bash
+++ aliases/available/general.aliases.bash
@@ -75,27 +75,6 @@ fi
 alias md='mkdir -p'
 alias rd='rmdir'
 
-# Common misspellings of bash-it
-alias shit='bash-it'
-alias batshit='bash-it'
-alias bashit='bash-it'
-alias batbsh='bash-it'
-alias babsh='bash-it'
-alias bash_it='bash-it'
-alias bash_ti='bash-it'
-
-# Additional bash-it aliases for help/show
-alias bshsa='bash-it show aliases'
-alias bshsc='bash-it show completions'
-alias bshsp='bash-it show plugins'
-alias bshha='bash-it help aliases'
-alias bshhc='bash-it help completions'
-alias bshhp='bash-it help plugins'
-alias bshsch="bash-it search"
-alias bshenp="bash-it enable plugin"
-alias bshena="bash-it enable alias"
-alias bshenc="bash-it enable completion"
-
 # Shorten extract
 alias xt="extract"
 
