b220c0e7441ab4e55c514b9d2348c103de0c4cd7
Uco Mesdag <uco.mesdag@allyourco.de>
2020-04-13 02:30:28 +0200

    Plugin to toggle sudo before your current or last command

---
+++ plugins/available/sudo.plugin.bash
@@ -0,0 +1,15 @@
+cite about-plugin
+about-plugin 'Toggle sudo before your current or last command'
+
+function sudo-command-line() {
+  [[ ${#READLINE_LINE} -eq 0 ]] && READLINE_LINE=$(fc -l -n -1 | xargs)
+  if [[ $READLINE_LINE == sudo\ * ]]; then
+    READLINE_LINE="${READLINE_LINE#sudo }"
+  else
+    READLINE_LINE="sudo $READLINE_LINE"
+  fi
+  READLINE_POINT=${#READLINE_LINE}
+}
+
+# Define shortcut keys: [Esc] [Esc]
+bind -x '"\e\e": sudo-command-line'
