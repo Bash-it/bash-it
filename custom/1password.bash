46d915cacdab207a98091c11ef3e63a1a72a9893
Adrian Cardenas <arcardenas@gmail.com>
2020-06-24 11:42:29 -0400

    add 1password session login function

--- custom/1password.bash
+++ custom/1password.bash
@@ -1,1 +1,7 @@
-alias op_session_login=eval $(op signin ${OP_ACCOUNT_NAME})
+#!/bin/bash
+
+function op_session_login {
+	local TOKEN=$(op signin ${OP_ACCOUNT_NAME} --raw)
+	export OP_SESSION_$OP_ACCOUNT_NAME=$TOKEN
+	env | grep OP_SESSION_
+}
