d46fc18bd4ed763c787767dcb45452dfb6cdd0d4
Noah Gorny <noah@gittabags.com>
2020-06-24 15:13:33 +0300

    alias: git: Add new pretty git log alias (ggf)

--- aliases/available/git.aliases.bash
+++ aliases/available/git.aliases.bash
@@ -69,6 +69,7 @@ alias gdel='git branch -D'
 alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias gg="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
+alias ggf="git log --graph --date=short --pretty=format:'%C(auto)%h %Cgreen%an%Creset %Cblue%cd%Creset %C(auto)%d %s'"
 alias ggs="gg --stat"
 alias gsh="git show"
 alias gsl="git shortlog -sn"
