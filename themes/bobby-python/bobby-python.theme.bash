6123af42187fd3893b483a12e02caa9c57b1d0e0
Zhengnan Zhao <zzhao212@gmail.com>
2020-10-01 23:40:59 -0400

    Make bobby and bobby-python themes more readable

--- themes/bobby-python/bobby-python.theme.bash
+++ themes/bobby-python/bobby-python.theme.bash
@@ -12,8 +12,13 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 CONDAENV_THEME_PROMPT_SUFFIX="|"
 
 function prompt_command() {
-    #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="\n${yellow}$(python_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+    PS1="\n${yellow}$(python_version_prompt) "  # Name of virtual env followed by python version
+    PS1+="${purple}\h "
+    PS1+="${reset_color}in "
+    PS1+="${green}\w\n"
+    PS1+="${bold_cyan}$(scm_char)"
+    PS1+="${green}$(scm_prompt_info) "
+    PS1+="${green}→${reset_color} "
 }
 
 safe_append_prompt_command prompt_command
