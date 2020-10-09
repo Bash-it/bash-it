c2566f26bd7bdeef919af1daaa7bee9c2c73b5bc
Noah Gorny <noah@gittabags.com>
2020-10-10 02:41:45 +0300

    completion: github: Use _log_warning instead of simple echo

--- completion/available/github-cli.completion.bash
+++ completion/available/github-cli.completion.bash
@@ -4,7 +4,7 @@ about-completion "GitHub CLI completion"
 
 if _command_exists gh; then
   if _command_exists brew; then
-    echo "You don't need github-cli completion enabled if you have system completion enabled"
+    _log_warning "You don't need github-cli completion enabled if you have system completion enabled"
   fi
   eval "$(gh completion --shell=bash)"
 fi
