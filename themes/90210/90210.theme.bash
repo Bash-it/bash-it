3f688170478571f2de2d5c0457c655f878f63fa3
Noah Gorny <noah@gittabags.com>
2020-10-28 21:04:04 +0200

    Clean 90210 theme

--- themes/90210/90210.theme.bash
+++ themes/90210/90210.theme.bash
@@ -10,8 +10,8 @@ GIT_THEME_PROMPT_PREFIX=" ${green}|"
 GIT_THEME_PROMPT_SUFFIX="${green}|"
 
 # Nicely formatted terminal prompt
-function prompt_command(){
-  export PS1="\n${bold_black}[${blue}\@${bold_black}]-${bold_black}[${green}\u${yellow}@${green}\h${bold_black}]-${bold_black}[${purple}\w${bold_black}]-$(scm_prompt_info)\n${reset_color}\$ "
+function prompt_command() {
+	export PS1="\n${bold_black}[${blue}\@${bold_black}]-${bold_black}[${green}\u${yellow}@${green}\h${bold_black}]-${bold_black}[${purple}\w${bold_black}]-$(scm_prompt_info)\n${reset_color}\$ "
 }
 
 safe_append_prompt_command prompt_command
