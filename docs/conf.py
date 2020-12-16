9ca6841e209693cf2a98106ac458b18c081d8b38
Noah Gorny <noah@gittabags.com>
2020-12-16 20:01:02 +0200

    docs: Add sphinx-emoji support

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinxemoji.sphinxemoji',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
