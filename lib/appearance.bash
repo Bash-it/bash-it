8087e7af49886aeb5cdb28d3f1a04093aac75453
cornfeedhobo <cornfeedhobo@fuzzlabs.org>
2021-01-03 23:19:53 -0600

    linting clean up

--- lib/appearance.bash
+++ lib/appearance.bash
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1090
 
 # colored ls
 export LSCOLORS='Gxfxcxdxdxegedabagacad'
 
 if [[ -z "$CUSTOM_THEME_DIR" ]]; then
-    CUSTOM_THEME_DIR="${BASH_IT_CUSTOM:=${BASH_IT}/custom}/themes"
+	CUSTOM_THEME_DIR="${BASH_IT_CUSTOM:=${BASH_IT}/custom}/themes"
 fi
 
 # Load the theme
-if [[ $BASH_IT_THEME ]]; then
-    if [[ -f $BASH_IT_THEME ]]; then
-        source $BASH_IT_THEME
-    elif [[ -f "$CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash" ]]; then
-        source "$CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
-    else
-        source "$BASH_IT/themes/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
-    fi
+if [[ "$BASH_IT_THEME" ]]; then
+	if [[ -f $BASH_IT_THEME ]]; then
+		source "$BASH_IT_THEME"
+	elif [[ -f "$CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash" ]]; then
+		source "$CUSTOM_THEME_DIR/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
+	else
+		source "$BASH_IT/themes/$BASH_IT_THEME/$BASH_IT_THEME.theme.bash"
+	fi
 fi
