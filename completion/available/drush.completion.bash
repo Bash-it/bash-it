c3d0a7478743e444dafac4748e24647087827c3a
Gurkirat Singh <tbhaxor@gmail.com>
2021-01-10 18:37:16 +0530

    removed deprecated completion of drush

--- completion/available/drush.completion.bash
+++
@@ -1,2 +0,0 @@
-_log_error 'Bash completion for "drush" is now deprecated, as it used code with incompatible license.
-Please disable this completion and use the instructions from "drush" developers instead.'
