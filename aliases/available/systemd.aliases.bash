f1a1730215879874d6ebb6e8277c8833cf9e878a
Artem Kovalov <artem.kovalyov@gmail.com>
2021-01-10 22:34:24 +0100

    Update aliases/available/systemd.aliases.bash

--- aliases/available/systemd.aliases.bash
+++ aliases/available/systemd.aliases.bash
@@ -14,7 +14,7 @@ case $OSTYPE in
 	alias sceu='systemctl --user stop'
 	alias scs='systemctl start'
 	alias scsu='systemctl --user start'
-# Keeping previous aliases for a non-braking change.
+# Keeping previous aliases for a non-breaking change.
 	alias scue='systemctl --user stop'
 	alias scus='systemctl --user start'
 	alias scr='systemctl daemon-reload'
