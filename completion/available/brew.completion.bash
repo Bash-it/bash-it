43ca459080fa81d39144745837b6107131e2dd6e
Gurkirat Singh <tbhaxor@gmail.com>
2021-01-10 19:20:30 +0530

    formatted brew completion

--- completion/available/brew.completion.bash
+++ completion/available/brew.completion.bash
@@ -4,8 +4,8 @@
 # BASH_IT_LOAD_PRIORITY: 375
 
 if [[ "$(uname -s)" != 'Darwin' ]] ; then
-  _log_warning "unsupported operating system - only 'Darwin' is supported"
-  return 0
+    _log_warning "unsupported operating system - only 'Darwin' is supported"
+    return 1
 fi
 
 # Make sure brew is installed
@@ -13,15 +13,15 @@ _command_exists brew || return 0
 
 BREW_PREFIX=${BREW_PREFIX:-$(brew --prefix)}
 
-if [[ -r "$BREW_PREFIX"/etc/bash_completion.d/brew ]] ; then
-  source "$BREW_PREFIX"/etc/bash_completion.d/brew
+if [[ -r "$BREW_PREFIX"/etc/bash_completion.d/brew ]]; then
+    source "$BREW_PREFIX"/etc/bash_completion.d/brew
 
-elif [[ -r "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh ]] ; then
-  source "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh
+    elif [[ -r "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh ]] ; then
+    source "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh
 
-elif [[ -f "$BREW_PREFIX"/completions/bash/brew ]] ; then
-  # For the git-clone based installation, see here for more info:
-  # https://github.com/Bash-it/bash-it/issues/1458
-  # https://docs.brew.sh/Shell-Completion
-  source "$BREW_PREFIX"/completions/bash/brew
+    elif [[ -f "$BREW_PREFIX"/completions/bash/brew ]] ; then
+    # For the git-clone based installation, see here for more info:
+    # https://github.com/Bash-it/bash-it/issues/1458
+    # https://docs.brew.sh/Shell-Completion
+    source "$BREW_PREFIX"/completions/bash/brew
 fi
