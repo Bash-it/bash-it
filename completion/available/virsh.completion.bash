800e754e0ce7e194f874bbb1e2dcfe3062b2adb7
Gurkirat Singh <tbhaxor@gmail.com>
2021-01-10 18:26:35 +0530

    removed deprecated bash completion of virsh

--- completion/available/virsh.completion.bash
+++
@@ -1,2 +0,0 @@
-_log_error 'Bash completion for "virsh" is now deprecated, as it used code with incompatible license.
-Please disable this completion and use the instructions from "virsh" developers instead.'
