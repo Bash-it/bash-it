6e9fa193b7dbb3341aceb137b206a775e08dec2c
Gurkirat Singh <tbhaxor@gmail.com>
2021-01-10 19:22:00 +0530

    formatted conda completion and used helper function

--- completion/available/conda.completion.bash
+++ completion/available/conda.completion.bash
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-which register-python-argcomplete > /dev/null \
-  && eval "$(register-python-argcomplete conda)" \
-  || echo "Please install argcomplete to use conda completion"
+
+if _command_exists conda
+then
+    if _command_exists register-python-argcomplete
+    then
+        eval "$(register-python-argcomplete conda)"
+    else
+        _log_warning "Argcomplete not found. Please run 'conda install argcomplete'"
+        return 1
+    fi
+fi
