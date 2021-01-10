325cda2838c4b76cf8e76232ac7a5ae0fd484452
Gurkirat Singh <tbhaxor@gmail.com>
2021-01-10 18:36:47 +0530

    removed deprecated completion of todo

--- completion/available/todo.completion.bash
+++
@@ -1,2 +0,0 @@
-_log_error 'Bash completion for "todo.txt-cli" is now deprecated, as it used code with incompatible license.
-Please disable this completion and use the instructions from "todo.txt-cli" developers instead.'
