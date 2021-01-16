2e3cf0b3c0c170ecb4ca103c0f593b71a14e9b3a
Noah Gorny <noah@gittabags.com>
2021-01-16 23:18:06 +0200

    completion: Lint docker completion and add about-completion

--- completion/available/docker.completion.bash
+++ completion/available/docker.completion.bash
@@ -1,21 +1,24 @@
-#!/usr/bin/env bash
+# shellcheck shell=bash
+cite "about-completion"
+about-completion "docker completion"
 
 # Make sure docker is installed
 _command_exists docker || return
 
 # Don't handle completion if it's already managed
-complete -p docker &>/dev/null && return
+_completion_exists docker && return
 
 _docker_bash_completion_paths=(
-  # MacOS
-  '/Applications/Docker.app/Contents/Resources/etc/docker.bash-completion'
-  # Linux
-  '/usr/share/bash-completion/completions/docker'
+	# MacOS
+	'/Applications/Docker.app/Contents/Resources/etc/docker.bash-completion'
+	# Linux
+	'/usr/share/bash-completion/completions/docker'
 )
 
-for fn in "${_docker_bash_completion_paths[@]}" ; do
-  if [ -r "$fn" ] ; then
-    source "$fn"
-    break
-  fi
+for fn in "${_docker_bash_completion_paths[@]}"; do
+	if [ -r "$fn" ]; then
+		# shellcheck disable=SC1090
+		source "$fn"
+		break
+	fi
 done
