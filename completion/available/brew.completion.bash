37a7fac128a8f16b7f750bf96ec5ef849383a013
Noah Gorny <noah@gittabags.com>
2021-01-22 23:44:41 +0200

    completion: brew: Add shellcheck ignore directives

--- completion/available/brew.completion.bash
+++ completion/available/brew.completion.bash
@@ -16,14 +16,17 @@ _command_exists brew || return 0
 BREW_PREFIX=${BREW_PREFIX:-$(brew --prefix)}
 
 if [[ -r "$BREW_PREFIX"/etc/bash_completion.d/brew ]]; then
+	# shellcheck disable=1090
 	source "$BREW_PREFIX"/etc/bash_completion.d/brew
 
 elif [[ -r "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh ]]; then
+	# shellcheck disable=1090
 	source "$BREW_PREFIX"/Library/Contributions/brew_bash_completion.sh
 
 elif [[ -f "$BREW_PREFIX"/completions/bash/brew ]]; then
 	# For the git-clone based installation, see here for more info:
 	# https://github.com/Bash-it/bash-it/issues/1458
 	# https://docs.brew.sh/Shell-Completion
+	# shellcheck disable=1090
 	source "$BREW_PREFIX"/completions/bash/brew
 fi
