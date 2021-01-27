c79d6cd609296b377faa8e71836d2318c890fb8e
Noah Gorny <noah@gittabags.com>
2021-01-27 22:51:43 +0200

    vendor: Add init.d directory to clean_files.txt
    Also fix and lint the directory.
    This is our code, and as such, should be linted.

--- vendor/init.d/preexec.bash
+++ vendor/init.d/preexec.bash
@@ -1,1 +1,3 @@
-source ${BASH_IT}/vendor/github.com/rcaloras/bash-preexec/bash-preexec.sh
+# shellcheck shell=bash
+# shellcheck disable=1090
+source "${BASH_IT}"/vendor/github.com/rcaloras/bash-preexec/bash-preexec.sh
