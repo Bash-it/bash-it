6866648ac65a8f18facbcc6c14985af8b5737d76
Titivoot Pangrit <tanlnwtitivoot@gmail.com>
2021-02-27 21:17:13 +0300

    Add theme Easy

---
+++ themes/easy/easy.theme.bash
@@ -0,0 +1,19 @@
+# shellcheck shell=bash
+
+SCM_THEME_PROMPT_PREFIX="${bold_green}[ ${normal}"
+SCM_THEME_PROMPT_SUFFIX="${bold_green} ] "
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+
+prompt_command() {
+	if [ "$(whoami)" = root ]; then
+		cursor_color="${bold_red}"
+		user_color="${green}"
+	else
+		cursor_color="${bold_green}"
+		user_color="${white}"
+	fi
+	PS1="${user_color}\u${normal}@${white}\h ${bold_black}\w\n${reset_color}$(scm_prompt_info)${cursor_color}❯ ${normal}"
+}
+
+safe_append_prompt_command prompt_command
