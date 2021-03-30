e481eb41e51471accd79dab887a22188ab1e43bd
Gurkirat Singh <tbhaxor@gmail.com>
2021-03-30 11:03:18 +0530

    completion (improvement): dmidecode lint fixed

--- completion/available/dmidecode.completion.bash
+++ completion/available/dmidecode.completion.bash
@@ -1,15 +1,19 @@
+# shellcheck shell=bash
+
 function __dmidecode_completion() {
 	local prev=$(_get_pword)
 	local curr=$(_get_cword)
 
 	case $prev in
-	-s | --string | -t | --type)
-		OPTS=$(dmidecode $prev 2>&1 | grep -E '^ ' | sed 's/ *//g')
-		COMPREPLY=($(compgen -W "$OPTS" -- "$curr"))
-		;;
-	dmidecode)
-		COMPREPLY=($(compgen -W "-d --dev-mem -h --help -q --quiet -s --string -t --type -H --handle -u --dump{,-bin} --from-dump --no-sysfs --oem-string -V --version" -- "$curr"))
-		;;
+		-s | --string | -t | --type)
+			OPTS=$(dmidecode "$prev" 2>&1 | grep -E '^ ' | sed 's/ *//g')
+			# shellcheck disable=SC2207
+			COMPREPLY=($(compgen -W "$OPTS" -- "$curr"))
+			;;
+		dmidecode)
+			# shellcheck disable=SC2207
+			COMPREPLY=($(compgen -W "-d --dev-mem -h --help -q --quiet -s --string -t --type -H --handle -u --dump{,-bin} --from-dump --no-sysfs --oem-string -V --version" -- "$curr"))
+			;;
 	esac
 }
 
