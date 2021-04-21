7b8dbd39bc821a5be25043375920823fd168e6ea
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2021-04-22 06:46:50 +0800

    Clean alias completion plugin (#1872)
    * Sort plugins/available
    
    * Clean alias-completion plugin
    
    * Fix shellcheck warning. SC1090
    
    * Change shellcheck source to /dev/null for explicity
    
    * Disable shellcheck warning SC2162

--- plugins/available/alias-completion.plugin.bash
+++ plugins/available/alias-completion.plugin.bash
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 # Load after the other completions to understand what needs to be completed
 # BASH_IT_LOAD_PRIORITY: 365
 
@@ -17,54 +18,61 @@ about-plugin 'Automatic completion of aliases'
 
 # Automatically add completion for all aliases to commands having completion functions
 function alias_completion {
-    local namespace="alias_completion"
+	local namespace="alias_completion"
 
-    # parse function based completion definitions, where capture group 2 => function and 3 => trigger
-    local compl_regex='complete( +[^ ]+)* -F ([^ ]+) ("[^"]+"|[^ ]+)'
-    # parse alias definitions, where capture group 1 => trigger, 2 => command, 3 => command arguments
-    local alias_regex="alias( -- | )([^=]+)='(\"[^\"]+\"|[^ ]+)(( +[^ ]+)*)'"
+	# parse function based completion definitions, where capture group 2 => function and 3 => trigger
+	local compl_regex='complete( +[^ ]+)* -F ([^ ]+) ("[^"]+"|[^ ]+)'
+	# parse alias definitions, where capture group 1 => trigger, 2 => command, 3 => command arguments
+	local alias_regex="alias( -- | )([^=]+)='(\"[^\"]+\"|[^ ]+)(( +[^ ]+)*)'"
 
-    # create array of function completion triggers, keeping multi-word triggers together
-    eval "local completions=($(complete -p | sed -Ene "/$compl_regex/s//'\3'/p"))"
-    (( ${#completions[@]} == 0 )) && return 0
+	# create array of function completion triggers, keeping multi-word triggers together
+	eval "local completions=($(complete -p | sed -Ene "/$compl_regex/s//'\3'/p"))"
+	((${#completions[@]} == 0)) && return 0
 
-    # create temporary file for wrapper functions and completions
-    local tmp_file; tmp_file="$(mktemp -t "${namespace}-${RANDOM}XXXXXX")" || return 1
+	# create temporary file for wrapper functions and completions
+	local tmp_file
+	tmp_file="$(mktemp -t "${namespace}-${RANDOM}XXXXXX")" || return 1
 
-    local completion_loader; completion_loader="$(complete -p -D 2>/dev/null | sed -Ene 's/.* -F ([^ ]*).*/\1/p')"
+	local completion_loader
+	completion_loader="$(complete -p -D 2> /dev/null | sed -Ene 's/.* -F ([^ ]*).*/\1/p')"
 
-    # read in "<alias> '<aliased command>' '<command args>'" lines from defined aliases
-    local line; while read line; do
-        eval "local alias_tokens; alias_tokens=($line)" 2>/dev/null || continue # some alias arg patterns cause an eval parse error
-        local alias_name="${alias_tokens[0]}" alias_cmd="${alias_tokens[1]}" alias_args="${alias_tokens[2]# }"
+	# read in "<alias> '<aliased command>' '<command args>'" lines from defined aliases
+	local line
 
-        # skip aliases to pipes, boolean control structures and other command lists
-        # (leveraging that eval errs out if $alias_args contains unquoted shell metacharacters)
-        eval "local alias_arg_words; alias_arg_words=($alias_args)" 2>/dev/null || continue
-        # avoid expanding wildcards
-        read -a alias_arg_words <<< "$alias_args"
+	# shellcheck disable=SC2162
+	# some aliases do have backslashes that needs to be interpreted
+	while read line; do
+		eval "local alias_tokens; alias_tokens=($line)" 2> /dev/null || continue # some alias arg patterns cause an eval parse error
+		local alias_name="${alias_tokens[0]}" alias_cmd="${alias_tokens[1]}" alias_args="${alias_tokens[2]# }"
 
-        # skip alias if there is no completion function triggered by the aliased command
-        if [[ ! " ${completions[*]} " =~ " $alias_cmd " ]]; then
-            if [[ -n "$completion_loader" ]]; then
-                # force loading of completions for the aliased command
-                eval "$completion_loader $alias_cmd"
-                # 124 means completion loader was successful
-                [[ $? -eq 124 ]] || continue
-                completions+=($alias_cmd)
-            else
-                continue
-            fi
-        fi
-        local new_completion="$(complete -p "$alias_cmd" 2>/dev/null)"
+		# skip aliases to pipes, boolean control structures and other command lists
+		# (leveraging that eval errs out if $alias_args contains unquoted shell metacharacters)
+		eval "local alias_arg_words; alias_arg_words=($alias_args)" 2> /dev/null || continue
+		# avoid expanding wildcards
+		read -ra alias_arg_words <<< "$alias_args"
 
-        # create a wrapper inserting the alias arguments if any
-        if [[ -n $alias_args ]]; then
-            local compl_func="${new_completion/#* -F /}"; compl_func="${compl_func%% *}"
-            # avoid recursive call loops by ignoring our own functions
-            if [[ "${compl_func#_$namespace::}" == $compl_func ]]; then
-                local compl_wrapper="_${namespace}::${alias_name}"
-                    echo "function $compl_wrapper {
+		# skip alias if there is no completion function triggered by the aliased command
+		if [[ ! " ${completions[*]} " =~ $alias_cmd ]]; then
+			if [[ -n "$completion_loader" ]]; then
+				# force loading of completions for the aliased command
+				eval "$completion_loader $alias_cmd"
+				# 124 means completion loader was successful
+				[[ $? -eq 124 ]] || continue
+				completions+=("$alias_cmd")
+			else
+				continue
+			fi
+		fi
+		local new_completion="$(complete -p "$alias_cmd" 2> /dev/null)"
+
+		# create a wrapper inserting the alias arguments if any
+		if [[ -n $alias_args ]]; then
+			local compl_func="${new_completion/#* -F /}"
+			compl_func="${compl_func%% *}"
+			# avoid recursive call loops by ignoring our own functions
+			if [[ "${compl_func#_$namespace::}" == "$compl_func" ]]; then
+				local compl_wrapper="_${namespace}::${alias_name}"
+				echo "function $compl_wrapper {
                         local compl_word=\$2
                         local prec_word=\$3
                         # check if prec_word is the alias itself. if so, replace it
@@ -81,15 +89,18 @@ function alias_completion {
                         (( COMP_POINT += \${#COMP_LINE} ))
                         $compl_func \"$alias_cmd\" \"\$compl_word\" \"\$prec_word\"
                     }" >> "$tmp_file"
-                    new_completion="${new_completion/ -F $compl_func / -F $compl_wrapper }"
-            fi
-        fi
+				new_completion="${new_completion/ -F $compl_func / -F $compl_wrapper }"
+			fi
+		fi
 
-        # replace completion trigger by alias
-        if [[ -n $new_completion ]]; then
-            new_completion="${new_completion% *} $alias_name"
-            echo "$new_completion" >> "$tmp_file"
-        fi
-    done < <(alias -p | sed -Ene "s/$alias_regex/\2 '\3' '\4'/p")
-    source "$tmp_file" && command rm -f "$tmp_file"
-}; alias_completion
+		# replace completion trigger by alias
+		if [[ -n $new_completion ]]; then
+			new_completion="${new_completion% *} $alias_name"
+			echo "$new_completion" >> "$tmp_file"
+		fi
+	done < <(alias -p | sed -Ene "s/$alias_regex/\2 '\3' '\4'/p")
+	# shellcheck source=/dev/null
+	source "$tmp_file" && command rm -f "$tmp_file"
+}
+
+alias_completion
