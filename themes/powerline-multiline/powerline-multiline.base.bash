9f4c71d40ff987eca27d53eabbe5c385c018cb9f
Ira Abramov <github@ira.abramov.org>
2021-05-19 10:54:28 +0300

    Add history flushes on each command to the powerline themes.

--- themes/powerline-multiline/powerline-multiline.base.bash
+++ themes/powerline-multiline/powerline-multiline.base.bash
@@ -60,6 +60,7 @@ function __powerline_prompt_command {
   SEGMENTS_AT_LEFT=0
   SEGMENTS_AT_RIGHT=0
   LAST_SEGMENT_COLOR=""
+  _save-and-reload-history "${HISTORY_AUTOSAVE:-0}"
 
   ## left prompt ##
   for segment in $POWERLINE_LEFT_PROMPT; do
