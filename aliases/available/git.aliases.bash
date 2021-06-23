ae186ceb0b8cee754740e1e6276102c8c574e71f
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-06-23 14:43:28 +0300

    another reorder

--- aliases/available/git.aliases.bash
+++ aliases/available/git.aliases.bash
@@ -17,7 +17,6 @@ alias gba='git branch -a'
 alias gbd='git branch -d'
 alias gbm='git branch -m'
 alias gbt='git branch --track'
-alias gbup='git log --branches --not --remotes --no-walk --decorate --oneline' # FROM https://stackoverflow.com/questions/39220870/in-git-list-names-of-branches-with-unpushed-commits
 alias gdel='git branch -D'
 
 # for-each-ref
@@ -72,6 +71,7 @@ alias gup='git fetch && git rebase'
 alias gg='git log --graph --pretty=format:'\''%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset'\'' --abbrev-commit --date=relative'
 alias ggf='git log --graph --date=short --pretty=format:'\''%C(auto)%h %Cgreen%an%Creset %Cblue%cd%Creset %C(auto)%d %s'\'''
 alias ggs='gg --stat'
+alias ggup='git log --branches --not --remotes --no-walk --decorate --oneline' # FROM https://stackoverflow.com/questions/39220870/in-git-list-names-of-branches-with-unpushed-commits
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias gnew='git log HEAD@{1}..HEAD@{0}' # Show commits since last pull, see http://blogs.atlassian.com/2014/10/advanced-git-aliases/
 alias gwc='git whatchanged'
