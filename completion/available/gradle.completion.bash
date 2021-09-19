679d8b10b6776e48843227f0f9327b5a3d7d697e
John D Pell <John+git@gaelicWizard.net>
2021-09-19 01:16:46 -0700

    completion/gradle: use `_command_exists`

--- completion/available/gradle.completion.bash
+++ completion/available/gradle.completion.bash
@@ -58,9 +58,9 @@ __gradle-set-cache-name() {
 
 __gradle-set-files-checksum() {
     # Cache MD5 sum of all Gradle scripts and modified timestamps
-    if builtin command -v md5 > /dev/null; then
+    if _command_exists md5; then
         gradle_files_checksum=$(md5 -q -s "$(cat "$cache_dir/$cache_name" | xargs ls -o 2>/dev/null)")
-    elif builtin command -v md5sum > /dev/null; then
+    elif _command_exists md5sum; then
         gradle_files_checksum=$(cat "$cache_dir/$cache_name" | xargs ls -o 2>/dev/null | md5sum | awk '{print $1}')
     else
         echo "Cannot generate completions as neither md5 nor md5sum exist on \$PATH"
