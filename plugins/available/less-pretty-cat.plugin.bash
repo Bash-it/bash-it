58576483773e3f90f0044b078f8a6d9b8b61a163
John D Pell <John+git@gaelicWizard.net>
2021-09-22 14:16:23 -0700

    plugins/less-pretty-cat: simplify code flow
    Convert from indented if-block to return then unindented code. This should have basically one line change at the top, one line removed at the bottom, and then all whitespace.

--- plugins/available/less-pretty-cat.plugin.bash
+++ plugins/available/less-pretty-cat.plugin.bash
@@ -2,30 +2,29 @@
 cite about-plugin
 about-plugin 'pygmentize instead of cat to terminal if possible'
 
-if _command_exists pygmentize
-then
-  # pigmentize cat and less outputs - call them ccat and cless to avoid that
-  # especially cat'ed output in scripts gets mangled with pygemtized meta characters
-  function ccat()
-  {
-      about 'runs either pygmentize or cat on each file passed in'
-      param '*: files to concatenate (as normally passed to cat)'
-      example 'cat mysite/manage.py dir/text-file.txt'
-		local file
-		: "${BASH_IT_CCAT_STYLE:=default}"
+_command_exists pygmentize || return
 
-      for file in "$@"
-      do
-          pygmentize -f 256 -O style="$BASH_IT_CCAT_STYLE" -g "$file" 2>/dev/null || command cat "$file";
-      done
-  }
+# pigmentize cat and less outputs - call them ccat and cless to avoid that
+# especially cat'ed output in scripts gets mangled with pygemtized meta characters
+function ccat() {
+	about 'runs either pygmentize or cat on each file passed in'
+	param '*: files to concatenate (as normally passed to cat)'
+	example 'ccat mysite/manage.py dir/text-file.txt'
 
-  function cless()
-  {
-      about 'pigments the file passed in and passes it to less for pagination'
-      param '$1: the file to paginate with less'
-      example 'less mysite/manage.py'
-		: "${BASH_IT_CLESS_STYLE:=default}"
-      pygmentize -f 256 -O style="$BASH_IT_CLESS_STYLE" -g "$@" | command less -R
-  }
-fi
+	local file
+	: "${BASH_IT_CCAT_STYLE:=default}"
+
+	for file in "$@"; do
+		pygmentize -f 256 -O style="$BASH_IT_CCAT_STYLE" -g "$file" 2> /dev/null || command cat "$file"
+	done
+}
+
+function cless() {
+	about 'pigments the file passed in and passes it to less for pagination'
+	param '1: the file to paginate with less'
+	example 'cless mysite/manage.py'
+
+	: "${BASH_IT_CLESS_STYLE:=default}"
+
+	pygmentize -f 256 -O style="$BASH_IT_CLESS_STYLE" -g "$@" | command less -R
+}
