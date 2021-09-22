6e2e945771fb4488e5bc1b875db5c9221c1fc102
John D Pell <John+git@gaelicWizard.net>
2021-09-22 13:21:16 -0700

    aliases/vim: simplify code flow

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -2,20 +2,14 @@
 cite 'about-alias'
 about-alias 'vim abbreviations'
 
-if _command_exists vim; then
-	alias v='$VIM'
-	# open the vim help in fullscreen incorporated from
-	# https://stackoverflow.com/a/4687513
-	alias vimh='${VIM} -c ":h | only"'
-fi
+_command_exists vim || return
+
+alias v='vim'
+# open the vim help in fullscreen incorporated from
+# https://stackoverflow.com/a/4687513
+alias vimh='vim -c ":h | only"'
 
 # open vim in new tab is taken from
 # http://stackoverflow.com/questions/936501/let-gvim-always-run-a-single-instancek
-case $OSTYPE in
-	darwin*)
-		_command_exists mvim && function mvimt { command mvim --remote-tab-silent "$@" || command mvim "$@"; }
-		;;
-	*)
-		_command_exists gvim && function gvimt { command gvim --remote-tab-silent "$@" || command gvim "$@"; }
-		;;
-esac
+_command_exists mvim && function mvimt { command mvim --remote-tab-silent "$@" || command mvim "$@"; }
+_command_exists gvim && function gvimt { command gvim --remote-tab-silent "$@" || command gvim "$@"; }
