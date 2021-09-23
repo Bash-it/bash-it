2ddb40751f5dcdfbeb196a6d24deac599f39411f
John D Pell <John+git@gaelicWizard.net>
2021-09-23 15:42:23 -0700

    plugin/less-pretty-cat: remove `|| cat`
    The logic to run `cat` if `pygmentize` fails seems useless, so just remove it.

--- plugins/available/less-pretty-cat.plugin.bash
+++ plugins/available/less-pretty-cat.plugin.bash
@@ -7,24 +7,17 @@ _command_exists pygmentize || return
 # pigmentize cat and less outputs - call them ccat and cless to avoid that
 # especially cat'ed output in scripts gets mangled with pygemtized meta characters
 function ccat() {
-	about 'runs either pygmentize or cat on each file passed in'
+	about 'runs pygmentize on each file passed in'
 	param '*: files to concatenate (as normally passed to cat)'
 	example 'ccat mysite/manage.py dir/text-file.txt'
 
-	local file
-	: "${BASH_IT_CCAT_STYLE:=default}"
-
-	for file in "$@"; do
-		pygmentize -f 256 -O style="$BASH_IT_CCAT_STYLE" -g "$file" 2> /dev/null || command cat "$file"
-	done
+	pygmentize -f 256 -O style="${BASH_IT_CCAT_STYLE:-default}" -g "$@"
 }
 
 function cless() {
-	about 'pigments the file passed in and passes it to less for pagination'
-	param '1: the file to paginate with less'
+	about 'pigments the files passed in and passes to less for pagination'
+	param '*: the files to paginate with less'
 	example 'cless mysite/manage.py'
 
-	: "${BASH_IT_CLESS_STYLE:=default}"
-
-	pygmentize -f 256 -O style="$BASH_IT_CLESS_STYLE" -g "$@" | command less -R
+	pygmentize -f 256 -O style="${BASH_IT_CLESS_STYLE:-default}" -g "$@" | command less -R
 }
