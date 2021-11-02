bdb52896600efe72e28d27c14086ba1bf3089069
cornfeedhobo <cornfeedhobo@fuzzlabs.org>
2021-11-01 22:26:50 -0500

    Wholesale cleanup of all history plugins
    - remove the use of redundant cite
    - minor cleanups to history.plugin.bash
    - set explicit load orders for history-search and history-substring-search
    - add new plugin history-eternal
    - replace superfluous trap for managing HISTTIMEFORMAT changes

--- plugins/available/history-substring-search.plugin.bash
+++ plugins/available/history-substring-search.plugin.bash
@@ -1,6 +1,8 @@
 # shellcheck shell=bash
-cite about-plugin
 about-plugin 'search history using the substring already entered'
+
+# Load after the history plugin
+# BASH_IT_LOAD_PRIORITY: 375
 
 # enter a few characters and press UpArrow/DownArrow
 # to search backwards/forwards through the history
