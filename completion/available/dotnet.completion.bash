77c5f8eda9c54b5d8744f64da8e0aacc21b05c7d
John D Pell <John+Git@gaelicWizard.net>
2022-01-12 22:38:51 -0800

    completion/dotnet: modernize
    and `shfmt`

--- completion/available/dotnet.completion.bash
+++ completion/available/dotnet.completion.bash
@@ -2,17 +2,13 @@
 about-completion "bash parameter completion for the dotnet CLI"
 # see https://docs.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete#bash
 
-_dotnet_bash_complete()
-{
-  local word=${COMP_WORDS[COMP_CWORD]}
+function _dotnet_bash_complete() {
+	local cur="${COMP_WORDS[COMP_CWORD]}" IFS=$'\n'
+	local candidates
 
-  local completions
-  completions="$(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)"
-  if [ $? -ne 0 ]; then
-    completions=""
-  fi
+	read -d '' -ra candidates < <(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2> /dev/null)
 
-  COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+	read -d '' -ra COMPREPLY < <(compgen -W "${candidates[*]:-}" -- "$cur")
 }
 
 complete -f -F _dotnet_bash_complete dotnet
