1ec8f9df6402f0f2d22a7a35528573c5aee8be94
Petar Nikolovski <petar.nikolovski@protonmail.com>
2022-01-25 20:59:06 +0100

    Add virtualenv prompt for robbyrussell theme

--- themes/robbyrussell/robbyrussell.theme.bash
+++ themes/robbyrussell/robbyrussell.theme.bash
@@ -18,8 +18,19 @@ function git_prompt_info() {
   echo -e "$SCM_PREFIX${bold_red}$SCM_BRANCH$SCM_STATE$SCM_SUFFIX"
 }
 
+function venv_prompt() {
+	python_venv=""
+	# Detect python venv
+	if [[ -n "${CONDA_DEFAULT_ENV}" ]]; then
+		python_venv="($PYTHON_VENV_CHAR${CONDA_DEFAULT_ENV}) "
+	elif [[ -n "${VIRTUAL_ENV}" ]]; then
+		python_venv="($PYTHON_VENV_CHAR$(basename "${VIRTUAL_ENV}")) "
+	fi
+	[[ -n "${python_venv}" ]] && echo "${python_venv}"
+}
+
 function prompt_command() {
-  PS1="${bold_green}➜  ${bold_cyan}\W${reset_color}$(scm_prompt_info)${normal} "
+  PS1="$(venv_prompt)${bold_green}➜  ${bold_cyan}\W${reset_color}$(scm_prompt_info)${normal} "
 }
 
 PROMPT_COMMAND=prompt_command
