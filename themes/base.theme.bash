11eb0c126a774a9589b70ff68d4b8e3717c7a10d
Petar Nikolovski <petar.nikolovski@protonmail.com>
2022-01-27 20:56:38 +0100

    Add conda_or_venv_prompt function

--- themes/base.theme.bash
+++ themes/base.theme.bash
@@ -626,13 +626,13 @@ function _save-and-reload-history() {
 	[[ $autosave -eq 1 ]] && history -a && history -c && history -r
 }
 
-function venv_prompt() {
+function conda_or_venv_prompt() {
 	local python_venv=""
 	if [[ -n "${CONDA_DEFAULT_ENV:-}" ]]; then
-		python_venv="${CONDA_DEFAULT_ENV}"
+		python_venv=$(condaenv_prompt)
 		PYTHON_VENV_CHAR=${CONDA_PYTHON_VENV_CHAR}
 	elif [[ -n "${VIRTUAL_ENV:-}" ]]; then
-		python_venv="${VIRTUAL_ENV##*/}"
+		python_venv=$(virtualenv_prompt)
 	fi
 	[[ -n "${python_venv}" ]] && echo "${PYTHON_VENV_CHAR}${python_venv}"
 }
