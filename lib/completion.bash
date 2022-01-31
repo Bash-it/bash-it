76774017b8e6b8f53d2fba87af3a2257ba972141
John D Pell <John+Git@gaelicWizard.net>
2022-01-30 16:26:20 -0800

    lib/completion

---
+++ lib/completion.bash
@@ -0,0 +1,26 @@
+# shellcheck shell=bash
+# Functions for working with _Bash_'s Programmable Completion
+
+#
+##
+# Testing Completion Functions
+##
+#
+
+#
+
+#
+##
+# Generating Completion Results
+##
+#
+
+#
+
+#
+##
+# Loading _Bash It_'s Completion Plugins
+##
+#
+
+#
