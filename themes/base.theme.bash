8ef853cf578b41d3a27e58cd673f7b8c09d6287c
John D Pell <John+Git@gaelicWizard.net>
2022-02-08 16:17:46 -0800

    lib/battery: rename `plugin/battery`
    This plugin *only* provides utility functions, so it has zero cost to just being enabled. This allows us to eliminate assumptions from `lib/theme` and several themes.

--- themes/base.theme.bash
+++ themes/base.theme.bash
@@ -558,23 +558,6 @@ function battery_char {
 	fi
 }
 
-if ! _command_exists battery_charge; then
-	# if user has installed battery plugin, skip this...
-	function battery_charge() {
-		# no op
-		echo -n
-	}
-fi
-
-# The battery_char function depends on the presence of the battery_percentage function.
-# If battery_percentage is not defined, then define battery_char as a no-op.
-if ! _command_exists battery_percentage; then
-	function battery_char() {
-		# no op
-		echo -n
-	}
-fi
-
 function aws_profile {
 	if [[ $AWS_DEFAULT_PROFILE ]]; then
 		echo -e "${AWS_DEFAULT_PROFILE}"
