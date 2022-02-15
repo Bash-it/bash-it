0a8af6947e40b8a2cd69f49e7d39f4b1af297434
John D Pell <John+Git@gaelicWizard.net>
2022-02-15 12:58:55 -0800

    uninstall: //echo/printf
    - Alsö, add implementation note at top.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-if [ -z "$BASH_IT" ]; then
-	BASH_IT="$HOME/.bash_it"
+#
+# Since we're uninstalling, avoid depending on any other part of _Bash It_.
+# I.e., hard-code colors (avoid `lib/colors.bash`), &c.
+
+: "${BASH_IT:=${HOME?}/.bash_it}"
+
+CONFIG_FILE=".bashrc"
+if [[ ! -e ~/.bashrc && -e ~/.bash_profile ]]; then
+	# legacy Mac or WSL or just no backup file
+	CONFIG_FILE=".bash_profile"
 fi
 
 case $OSTYPE in
@@ -18,21 +26,19 @@ CONFIG_FILE="${BASH_IT_CONFIG_FILE:-"${CONFIG_FILE}"}"
 BACKUP_FILE=$CONFIG_FILE.bak
 
 if [[ ! -e "${HOME?}/$BACKUP_FILE" ]]; then
-	echo -e "\033[0;33mBackup file ${HOME?}/$BACKUP_FILE not found.\033[0m" >&2
+	printf '\e[0;33m%s\e[0m\n' "Backup file ~/$BACKUP_FILE not found."
 
 	test -w "${HOME?}/$CONFIG_FILE" \
 		&& mv "${HOME?}/$CONFIG_FILE" "${HOME?}/$CONFIG_FILE.uninstall" \
-		&& echo -e "\033[0;32mMoved your ${HOME?}/$CONFIG_FILE to ${HOME?}/$CONFIG_FILE.uninstall.\033[0m"
+		&& printf '\e[0;32m%s\e[0m\n' "Moved your ~/$CONFIG_FILE to ~/$CONFIG_FILE.uninstall."
 else
 	test -w "${HOME?}/$BACKUP_FILE" \
 		&& cp -a "${HOME?}/$BACKUP_FILE" "${HOME?}/$CONFIG_FILE" \
 		&& rm "${HOME?}/$BACKUP_FILE" \
-		&& echo -e "\033[0;32mYour original $CONFIG_FILE has been restored.\033[0m"
+		&& printf '\e[0;32m%s\e[0m\n' "Your original ~/$CONFIG_FILE has been restored."
 fi
 
-echo ""
-echo -e "\033[0;32mUninstallation finished successfully! Sorry to see you go!\033[0m"
-echo ""
-echo "Final steps to complete the uninstallation:"
-echo "  -> Remove the $BASH_IT folder"
-echo "  -> Open a new shell/tab/terminal"
+printf '\n\e[0;32m%s\e[0m\n\n' "Uninstallation finished successfully! Sorry to see you go!"
+printf '%s\n' "Final steps to complete the uninstallation:"
+printf '\t%s\n' "-> Remove the ${BASH_IT//${HOME?}/\~} folder"
+printf '\t%s\n' "-> Open a new shell/tab/terminal"
