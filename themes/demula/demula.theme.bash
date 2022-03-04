7a9420ec9b4eddc344a2679ad735d09ed6e9a605
John D Pell <John+git@gaelicWizard.net>
2022-03-04 12:37:15 -0800

    lib/battery: rename `plugin/battery`
    This plugin *only* provides utility functions, so it has zero cost to just being enabled. This allows us to eliminate assumptions from `lib/theme` and several themes.

--- themes/demula/demula.theme.bash
+++ themes/demula/demula.theme.bash
@@ -82,14 +82,6 @@ ${D_BRANCH_COLOR}%b %r ${D_CHANGES_COLOR}%m%u ${D_DEFAULT_COLOR}"
   fi
 }
 
-# checks if the plugin is installed before calling battery_charge
-safe_battery_charge() {
-  if _command_exists battery_charge ;
-  then
-    battery_charge
-  fi
-}
-
 # -------------------------------------------------------------- PROMPT OUTPUT
 prompt() {
   local LAST_COMMAND_FAILED=$(mitsuhikos_lastcommandfailed)
@@ -101,7 +93,7 @@ prompt() {
   if [[ "$OSTYPE" = 'linux'* ]]
   then
     PS1="${TITLEBAR}${SAVE_CURSOR}${MOVE_CURSOR_RIGHTMOST}${MOVE_CURSOR_5_LEFT}
-$(safe_battery_charge)${RESTORE_CURSOR}\
+$(battery_charge)${RESTORE_CURSOR}\
 ${D_USER_COLOR}\u ${D_INTERMEDIATE_COLOR}\
 at ${D_MACHINE_COLOR}\h ${D_INTERMEDIATE_COLOR}\
 in ${D_DIR_COLOR}\w ${D_INTERMEDIATE_COLOR}\
@@ -117,7 +109,7 @@ in ${D_DIR_COLOR}\w ${D_INTERMEDIATE_COLOR}\
 ${LAST_COMMAND_FAILED}\
 $(demula_vcprompt)\
 $(is_vim_shell)\
-$(safe_battery_charge)
+$(battery_charge)
 ${D_INTERMEDIATE_COLOR}$ ${D_DEFAULT_COLOR}"
   fi
 
