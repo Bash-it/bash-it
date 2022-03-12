b7c91850234e51ba04c8926b04e3c16daefddaa8
Gurkirat Singh <tbhaxor@gmail.com>
2022-03-13 04:05:02 +0530

    fix: SC1091 in apm completions

--- completion/available/apm.completion.bash
+++ completion/available/apm.completion.bash
@@ -1,4 +1,4 @@
 # shellcheck shell=bash
 about-completion "apm completion"
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC1091
 source "${BASH_IT}"/vendor/github.com/vigo/apm-bash-completion/apm
