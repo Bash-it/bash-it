2710d0c0c0b8f7aa627177de0135447ce844911d
Shoshta73 <128172135+Shoshta73@users.noreply.github.com>
2023-10-19 02:11:10 +0200

    add tmux kill session and detach aliases

--- aliases/available/tmux.aliases.bash
+++ aliases/available/tmux.aliases.bash
@@ -1,6 +1,8 @@
 # shellcheck shell=bash
 about-alias 'Tmux terminal multiplexer'
 
+alias td='tmux detach'
+alias tks='tmux kill-session -t'
 alias txl='tmux ls'
 alias txn='tmux new -s'
 alias txa='tmux a -t'
