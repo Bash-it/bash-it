4bd2aeabaf066768fe1c0281644b4a62a7a2c222
Ira Abramov <ira@agrematch.com>
2025-01-22 12:14:02 +0200

    ironing this odd lint problem

--- plugins/available/z_autoenv.plugin.bash
+++ plugins/available/z_autoenv.plugin.bash
@@ -1,45 +1,43 @@
+# shellcheck shell=bash
 cite about-plugin
 about-plugin 'source into environment when cding to directories'
 
-if [[ -n "${ZSH_VERSION}" ]]
-then __array_offset=0
-else __array_offset=1
+if [[ -n "${ZSH_VERSION}" ]]; then
+	__array_offset=0
+else
+	__array_offset=1
 fi
 
-autoenv_init()
-{
-  typeset target home _file
-  typeset -a _files
-  target=$1
-  home="${HOME%/*}"
+autoenv_init() {
+	typeset home _file # target
+	typeset -a _files
+	#target=$1
+	home="${HOME%/*}"
 
-  _files=( $(
-    while [[ "$PWD" != "/" && "$PWD" != "$home" ]]
-    do
-      _file="$PWD/.env"
-      if [[ -e "${_file}" ]]
-      then echo "${_file}"
-      fi
-      builtin cd ..
-    done
-  ) )
+	while [[ "$PWD" != "/" && "$PWD" != "$home" ]]; do
+		_file="$PWD/.env"
+		if [[ -e "${_file}" ]]; then
+			_files+=("${_file}")
+		fi
+		builtin cd .. || true
+	done
 
-  _file=${#_files[@]}
-  while (( _file > 0 ))
-  do
-    source "${_files[_file-__array_offset]}"
-    : $(( _file -= 1 ))
-  done
+	_file=${#_files[@]}
+	while ((_file > 0)); do
+		#shellcheck disable=SC1090
+		source "${_files[_file - __array_offset]}"
+		: $((_file -= 1))
+	done
 }
 
-cd()
-{
-  if builtin cd "$@"
-  then
-    autoenv_init
-    return 0
-  else
-    echo "else?"
-    return $?
-  fi
+cd() {
+	if builtin cd "$@"; then
+		autoenv_init
+		return 0
+	else
+		local return_code
+		return_code=$?
+		echo "else?"
+		return $return_code
+	fi
 }
