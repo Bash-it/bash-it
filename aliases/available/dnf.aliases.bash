5b16ce4a7be6417fe97babf879d87ab6043248d6
shine <git@shinenelson.com>
2025-02-07 14:42:33 +0530

    re-order list for aesthetics

--- aliases/available/dnf.aliases.bash
+++ aliases/available/dnf.aliases.bash
@@ -2,18 +2,18 @@
 about-alias 'dnf aliases for fedora 22+ distros'
 
 if _command_exists dnf; then
+	alias dnfp="dnf info"            # Show package information
 	alias dnfl="dnf list"            # List packages
 	alias dnfli="dnf list installed" # List installed packages
 	alias dnfgl="dnf grouplist"      # List package groups
 	alias dnfmc="dnf makecache"      # Generate metadata cache
-	alias dnfp="dnf info"            # Show package information
 	alias dnfs="dnf search"          # Search package
 
-	alias dnfu="sudo dnf upgrade"       # Upgrade package
 	alias dnfi="sudo dnf install"       # Install package
-	alias dnfri='sudo dnf reinstall'    # Reinstall package
-	alias dnfgi="sudo dnf groupinstall" # Install package group
 	alias dnfr="sudo dnf remove"        # Remove package
-	alias dnfgr="sudo dnf groupremove"  # Remove package group
+	alias dnfu="sudo dnf upgrade"       # Upgrade package
 	alias dnfc="sudo dnf clean all"     # Clean cache
+	alias dnfri="sudo dnf reinstall"    # Reinstall package
+	alias dnfgi="sudo dnf groupinstall" # Install package group
+	alias dnfgr="sudo dnf groupremove"  # Remove package group
 fi
