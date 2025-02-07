1f205695959e5cd22906a1f4ca8be6b06d8fee7b
shine <git@shinenelson.com>
2025-02-07 14:46:14 +0530

    re-order list for aesthetics

--- aliases/available/homebrew.aliases.bash
+++ aliases/available/homebrew.aliases.bash
@@ -2,14 +2,14 @@
 about-alias 'Some aliases for Homebrew'
 
 if _command_exists brew; then
-	alias bup='brew update && brew upgrade'
-	alias bout='brew outdated'
-	alias bin='brew install'
-	alias brm='brew uninstall'
-	alias bcl='brew cleanup'
+	alias bed='brew edit'
 	alias bls='brew list'
 	alias bsr='brew search'
-	alias binf='brew info'
 	alias bdr='brew doctor'
-	alias bed='brew edit'
+	alias bin='brew install'
+	alias bcl='brew cleanup'
+	alias brm='brew uninstall'
+	alias bout='brew outdated'
+	alias binf='brew info'
+	alias bup='brew update && brew upgrade'
 fi
