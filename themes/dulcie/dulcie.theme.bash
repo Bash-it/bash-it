c98df9715ccbfbc6f1f937dba6857993b9ed23d7
Edwin Kofler <edwin@kofler.dev>
2025-03-07 00:27:31 -0800

    Clean doubletime variants, dulcie, and duru

--- themes/dulcie/dulcie.theme.bash
+++ themes/dulcie/dulcie.theme.bash
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+# shellcheck shell=bash
+# shellcheck disable=SC2034 # Expected behavior for themes.
 
 # Simplistic one-liner theme to display source control management info beside
 # the ordinary Linux bash prompt.
@@ -25,7 +26,7 @@ dulcie_background() {
 
 dulcie_prompt() {
 	color_user_root=$(dulcie_color 169)
-	color_user_nonroot="${green}"
+	color_user_nonroot="${green?}"
 	color_host_local=$(dulcie_color 230)
 	color_host_remote=$(dulcie_color 214)
 	color_rootdir=$(dulcie_color 117)
@@ -48,18 +49,18 @@ dulcie_prompt() {
 			color_host="${color_host_local}"
 		fi
 
-		DULCIE_USER="${color_user}\u${reset_color}"
-		DULCIE_HOST="${color_host}\h${reset_color}"
-		DULCIE_WORKINGDIR="${color_workingdir}\W${reset_color}"
-		DULCIE_PROMPTCHAR="${color_user}"'\$'"${reset_color}"
+		DULCIE_USER="${color_user?}\u${reset_color?}"
+		DULCIE_HOST="${color_host?}\h${reset_color?}"
+		DULCIE_WORKINGDIR="${color_workingdir?}\W${reset_color?}"
+		DULCIE_PROMPTCHAR="${color_user?}"'\$'"${reset_color?}"
 
-		SCM_THEME_PROMPT_DIRTY=" ${red}✗${reset_color}"
-		SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
+		SCM_THEME_PROMPT_DIRTY=" ${red?}✗${reset_color}"
+		SCM_THEME_PROMPT_CLEAN=" ${bold_green?}✓${normal?}"
 		DULCIE_SCM_BACKGROUND="${background_scm}"
 		DULCIE_SCM_DIR_COLOR="${color_rootdir}"
 		SCM_THEME_ROOT_SUFFIX="${reset_color}${SCM_THEME_ROOT_SUFFIX}"
-		SCM_THEME_PROMPT_DIRTY=" $(dulcie_color 1)✗${reset_color}"
-		SCM_THEME_PROMPT_CLEAN=" $(dulcie_color 10)✓${reset_color}"
+		SCM_THEME_PROMPT_DIRTY=" $(dulcie_color 1)✗${reset_color?}"
+		SCM_THEME_PROMPT_CLEAN=" $(dulcie_color 10)✓${reset_color?}"
 	else
 		DULCIE_USER='\u'
 		DULCIE_HOST='\h'
@@ -79,9 +80,9 @@ dulcie_prompt() {
 	# Open the new terminal in the same directory
 	_is_function __vte_osc7 && __vte_osc7
 
-	PS1="${reset_color}[${DULCIE_USER}@${DULCIE_HOST}$(scm_prompt_info)${reset_color} ${DULCIE_WORKINGDIR}]"
+	PS1="${reset_color}[${DULCIE_USER}@${DULCIE_HOST}$(scm_prompt_info)${reset_color?} ${DULCIE_WORKINGDIR}]"
 	if [[ "${DULCIE_MULTILINE}" -eq "1" ]]; then
-		PS1="${reset_color}[${DULCIE_USER}@${DULCIE_HOST}${reset_color} ${DULCIE_WORKINGDIR}]"
+		PS1="${reset_color}[${DULCIE_USER}@${DULCIE_HOST}${reset_color?} ${DULCIE_WORKINGDIR}]"
 		if [[ "$(scm_prompt_info)" ]]; then
 			SCM_THEME_PROMPT_PREFIX="${DULCIE_SCM_BACKGROUND}|${DULCIE_SCM_DIR_COLOR}"
 			SCM_THEME_PROMPT_SUFFIX="|${normal}"
@@ -90,7 +91,7 @@ dulcie_prompt() {
 	else
 		SCM_THEME_PROMPT_PREFIX=" ${DULCIE_SCM_BACKGROUND}|${DULCIE_SCM_DIR_COLOR}"
 		SCM_THEME_PROMPT_SUFFIX="|${normal}"
-		PS1="${reset_color}[${DULCIE_USER}@${DULCIE_HOST}$(scm_prompt_info)${reset_color} ${DULCIE_WORKINGDIR}]"
+		PS1="${reset_color?}[${DULCIE_USER}@${DULCIE_HOST}$(scm_prompt_info)${reset_color?} ${DULCIE_WORKINGDIR}]"
 	fi
 	PS1="${PS1}${DULCIE_PROMPTCHAR} "
 }
--- themes/duru/duru.theme.bash
+++ themes/duru/duru.theme.bash
@@ -1,23 +1,25 @@
-#!/usr/bin/env bash
+# shellcheck shell=bash
+# shellcheck disable=SC2034 # Expected behavior for themes.
 
-SCM_THEME_PROMPT_PREFIX="${cyan} on ${green}"
+SCM_THEME_PROMPT_PREFIX="${cyan?} on ${green?}"
 SCM_THEME_PROMPT_SUFFIX=""
-SCM_THEME_PROMPT_DIRTY=" ${red}with changes"
+SCM_THEME_PROMPT_DIRTY=" ${red?}with changes"
 SCM_THEME_PROMPT_CLEAN=""
 
 venv() {
-	if [ ! -z "$VIRTUAL_ENV" ]; then
+	if [ -n "$VIRTUAL_ENV" ]; then
 		local env=$VIRTUAL_ENV
-		echo "${gray} in ${orange}${env##*/} "
+		echo "${gray?} in ${orange?}${env##*/} "
 	fi
 }
 
 last_two_dirs() {
-	pwd | rev | awk -F / '{print $1,$2}' | rev | sed s_\ _/_ | sed "s|$(sed 's,\/,,' <<< $HOME)|~|g"
+	# shellcheck disable=SC2001
+	pwd | rev | awk -F / '{print $1,$2}' | rev | sed s_\ _/_ | sed "s|$(sed 's,\/,,' <<< "$HOME")|~|g"
 }
 
 prompt() {
-	PS1="${yellow}# ${reset_color}$(last_two_dirs)$(scm_prompt_info)${reset_color}$(venv)${reset_color} ${cyan}\n> ${reset_color}"
+	PS1="${yellow?}# ${reset_color?}$(last_two_dirs)$(scm_prompt_info)${reset_color?}$(venv)${reset_color?} ${cyan?}\n> ${reset_color?}"
 }
 
 safe_append_prompt_command prompt
