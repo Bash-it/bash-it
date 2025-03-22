6bd7daa27b5f5f1f8d812b204002aec6588183e4
Edwin Kofler <edwin@kofler.dev>
2025-03-22 06:41:51 -0700

    Fix final errors

--- themes/cooperkid/cooperkid.theme.bash
+++ themes/cooperkid/cooperkid.theme.bash
@@ -30,7 +30,7 @@ function git_short_sha() {
 function prompt() {
 	local return_status=""
 	local ruby
-	ruby="${red}$(ruby_version_prompt)${reset_color}"
+	ruby="${red?}$(ruby_version_prompt)${reset_color?}"
 	local user_host="${green?}\h @ \w${reset_color?}"
 	local git_branch
 	git_branch="$(git_short_sha)${cyan?}$(scm_prompt_info)${reset_color?}"
