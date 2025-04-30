a7c308a96d41c608c2a89db15fa9fda0fc7251bd
Ira Abramov <github@ira.abramov.org>
2025-04-30 20:54:12 +0300

    clean up themes/powerline-naked

--- themes/powerline-naked/powerline-naked.base.bash
+++ themes/powerline-naked/powerline-naked.base.bash
@@ -1,12 +1,11 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034,SC1091 # Expected behavior for themes.
-# shellcheck source-path=themes/powerline/powerline.base.bash
 source "${BASH_IT?}/themes/powerline/powerline.base.bash"
 
 function __powerline_left_segment {
-	local OLD_IFS="${IFS}"
-	IFS="|"
-	local params=("$1")
+	local OLD_IFS="${IFS}" params=()
+	# shellcheck disable=SC2206 # not needed because we are splitting on "|"
+	IFS="|" params=($1)
 	IFS="${OLD_IFS}"
 	local separator=""
 	local pad_before_segment=" "
