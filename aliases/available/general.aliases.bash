e829b51c948f6f709914b6cab81e7b8dd74119ab
Ira Abramov <ira@agrematch.com>
2025-05-04 13:55:55 +0300

    general aliases: pointing at /home/$USER is a wild assumption, corrected to $HOME

--- aliases/available/general.aliases.bash
+++ aliases/available/general.aliases.bash
@@ -50,12 +50,12 @@ alias ipy='ipython'
 
 alias piano='pianobar'
 
-alias ..='cd ..'                     # Go up one directory
-alias cd..='cd ..'                   # Common misspelling for going up one directory
-alias ...='cd ../..'                 # Go up two directories
-alias ....='cd ../../..'             # Go up three directories
-alias -- -='cd -'                    # Go back
-alias dow='cd /home/$USER/Downloads' # Go to the Downloads directory
+alias ..='cd ..'               # Go up one directory
+alias cd..='cd ..'             # Common misspelling for going up one directory
+alias ...='cd ../..'           # Go up two directories
+alias ....='cd ../../..'       # Go up three directories
+alias -- -='cd -'              # Go back
+alias dow='cd $HOME/Downloads' # Go to the Downloads directory
 
 # Shell History
 alias h='history'
