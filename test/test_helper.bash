205a9d37e43bcdf2aeb5673024488f99823cd657
Ira Abramov <github@ira.abramov.org>
2025-10-15 00:03:51 +0300

    another shellcheck annoyance

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -45,6 +45,7 @@ function common_setup_file() {
 
 	load "${BASH_IT?}/vendor/github.com/erichs/composure/composure.sh"
 	# Extend composure with additional metadata functions
+	# shellcheck disable=SC2317
 	url() { :; }
 	# support 'plumbing' metadata
 	cite _about _param _example _group _author _version url
